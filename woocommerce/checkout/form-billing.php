e8d04a30f53b9b7964406c77d9dd6b276636e505
Chris Rault <chris@devignerforhire.com>
2015-11-25 11:09:22 +0200

    Rewrote form-billing.php with Beans markup

--- woocommerce/checkout/form-billing.php
+++ woocommerce/checkout/form-billing.php
@@ -7,20 +7,30 @@
  * @version     2.1.2
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 /** @global WC_Checkout $checkout */
+
 ?>
-<div class="woocommerce-billing-fields">
+
+
+<?php echo beans_open_markup( 'woo_billing_fields', 'div', array( 'class' => 'woocommerce-billing-fields' ) ); ?>
+
 	<?php if ( WC()->cart->ship_to_billing_address_only() && WC()->cart->needs_shipping() ) : ?>
 
-		<h3><?php _e( 'Billing &amp; Shipping', 'woocommerce' ); ?></h3>
+		<?php echo beans_open_markup( 'woo_billing_title', 'h3' ); ?>
+
+			<?php _e( 'Billing &amp; Shipping', 'woocommerce' ); ?>
+
+		<?php echo beans_close_markup( 'woo_billing_title', 'h3' ); ?>
 
 	<?php else : ?>
 
-		<h3><?php _e( 'Billing Details', 'woocommerce' ); ?></h3>
+		<?php echo beans_open_markup( 'woo_billing_title', 'h3' ); ?>
+
+			<?php _e( 'Billing Details', 'woocommerce' ); ?>
+
+		<?php echo beans_close_markup( 'woo_billing_title', 'h3' ); ?>
 
 	<?php endif; ?>
 
@@ -38,9 +48,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		<?php if ( $checkout->enable_guest_checkout ) : ?>
 
-			<p class="form-row form-row-wide create-account">
-				<input class="input-checkbox" id="createaccount" <?php checked( ( true === $checkout->get_value( 'createaccount' ) || ( true === apply_filters( 'woocommerce_create_account_default_checked', false ) ) ), true) ?> type="checkbox" name="createaccount" value="1" /> <label for="createaccount" class="checkbox"><?php _e( 'Create an account?', 'woocommerce' ); ?></label>
-			</p>
+			<?php echo beans_open_markup( 'woo_billing_register_input_wrap', 'p', array( 'class' => 'form-row form-row-wide create-account' ) ); ?>
+
+				<?php echo beans_selfclose_markup( 'woo_billing_register_input', 'input', array(
+					'class' => 'input-checkbox',
+					'id' => 'createaccount',
+					'checked' => true === $checkout->get_value( 'createaccount' ) || true === apply_filters( 'woocommerce_create_account_default_checked', false ) ? 'checked' : null,
+					'type' => 'checkbox',
+					'name' => 'createaccount',
+					'value' => 1 )
+				); ?>
+
+				<?php echo beans_open_markup( 'woo_billing_register_label', 'label', array(
+					'for' => 'createaccount',
+					'class' => 'checkbox'
+				) ); ?>
+
+					<?php _e( 'Create an account?', 'woocommerce' ); ?>
+
+				<?php echo beans_close_markup( 'woo_billing_register_label', 'label' ); ?>
+
+			<?php echo beans_close_markup( 'woo_billing_register_input_wrap', 'p' ); ?>
 
 		<?php endif; ?>
 
@@ -48,9 +76,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		<?php if ( ! empty( $checkout->checkout_fields['account'] ) ) : ?>
 
-			<div class="create-account">
+			<?php echo beans_open_markup( 'woo_billing_register', 'div', array( 'class' => 'create-account' ) ); ?>
 
-				<p><?php _e( 'Create an account by entering the information below. If you are a returning customer please login at the top of the page.', 'woocommerce' ); ?></p>
+				<?php echo beans_open_markup( 'woo_billing_register_note', 'p' ); ?>
+
+					<?php _e( 'Create an account by entering the information below. If you are a returning customer please login at the top of the page.', 'woocommerce' ); ?>
+
+				<?php echo beans_close_markup( 'woo_billing_register_note', 'p' ); ?>
 
 				<?php foreach ( $checkout->checkout_fields['account'] as $key => $field ) : ?>
 
@@ -58,13 +90,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 				<?php endforeach; ?>
 
-				<div class="clear"></div>
+				<?php echo beans_open_markup( 'woo_billing_register_clear', 'div', array( 'class' => 'clear' ) ); ?>
 
-			</div>
+				<?php echo beans_close_markup( 'woo_billing_register_clear', 'div' ); ?>
+
+			<?php echo beans_close_markup( 'woo_billing_register', 'div' ); ?>
 
 		<?php endif; ?>
 
 		<?php do_action( 'woocommerce_after_checkout_registration_form', $checkout ); ?>
 
 	<?php endif; ?>
-</div>
+
+<?php echo beans_close_markup( 'woo_billing_fields', 'div' ); ?>
