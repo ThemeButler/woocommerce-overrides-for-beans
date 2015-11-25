2f94eef3d38ed6eb07cd63e7c823ef9c28c6daf5
Chris Rault <chris@devignerforhire.com>
2015-11-25 11:06:43 +0200

    Rewrote cart-errors.php with Beans markup

--- woocommerce/checkout/cart-errors.php
+++ woocommerce/checkout/cart-errors.php
@@ -7,16 +7,29 @@
  * @version 2.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 ?>
 
 <?php wc_print_notices(); ?>
 
-<p><?php _e( 'There are some issues with the items in your cart (shown above). Please go back to the cart page and resolve these issues before checking out.', 'woocommerce' ) ?></p>
+<?php echo beans_open_markup( 'woo_checkout_cart_error', 'p' ); ?>
+
+	<?php _e( 'There are some issues with the items in your cart (shown above). Please go back to the cart page and resolve these issues before checking out.', 'woocommerce' ) ?>
+
+<?php echo beans_close_markup( 'woo_checkout_cart_error', 'p' ); ?>
 
 <?php do_action( 'woocommerce_cart_has_errors' ); ?>
 
-<p><a class="button wc-backward" href="<?php echo esc_url( wc_get_page_permalink( 'cart' ) ); ?>"><?php _e( 'Return To Cart', 'woocommerce' ) ?></a></p>
+<?php echo beans_open_markup( 'woo_checkout_back_to_cart', 'p' ); ?>
+
+	<?php echo beans_open_markup( 'woo_checkout_back_to_cart_link', 'a', array(
+		'class' => 'button wc-backward',
+		'href' => echo esc_url( wc_get_page_permalink( 'cart' ) )
+	) ); ?>
+
+		<?php _e( 'Return To Cart', 'woocommerce' ) ?>
+
+	<?php echo beans_close_markup( 'woo_checkout_back_to_cart_link', 'a' ); ?>
+
+<?php echo beans_close_markup( 'woo_checkout_back_to_cart', 'p' ); ?>
