e750ed63db8bf2380d9258c1a928600e01bc18e2
Chris Rault <chris@devignerforhire.com>
2015-11-25 11:19:25 +0200

    Rewrote form-login.php with Beans markup

--- woocommerce/checkout/form-login.php
+++ woocommerce/checkout/form-login.php
@@ -7,21 +7,23 @@
  * @version     2.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-if ( is_user_logged_in() || 'no' === get_option( 'woocommerce_enable_checkout_login_reminder' ) ) {
+if ( is_user_logged_in() || 'no' === get_option( 'woocommerce_enable_checkout_login_reminder' ) ) :
+
 	return;
-}
+
+endif;
 
 $info_message  = apply_filters( 'woocommerce_checkout_login_message', __( 'Returning customer?', 'woocommerce' ) );
-$info_message .= ' <a href="#" class="showlogin">' . __( 'Click here to login', 'woocommerce' ) . '</a>';
+
+$info_message .= beans_open_markup( 'woo_cart_login_form_login_link', 'a', array( 'href' => '#', 'class' => 'showlogin' ) ) . __( 'Click here to login', 'woocommerce' ) . beans_close_markup( 'woo_cart_login_form_login_link', 'a' );
+
 wc_print_notice( $info_message, 'notice' );
+
 ?>
 
-<?php
-	woocommerce_login_form(
+<?php woocommerce_login_form(
 		array(
 			'message'  => __( 'If you have shopped with us before, please enter your details in the boxes below. If you are a new customer please proceed to the Billing &amp; Shipping section.', 'woocommerce' ),
 			'redirect' => wc_get_page_permalink( 'checkout' ),
