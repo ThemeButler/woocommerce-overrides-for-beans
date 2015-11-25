3602583b34728e5987b010e6194c9003c032de8d
Chris Rault <chris@devignerforhire.com>
2015-11-25 09:34:45 +0200

    Rewrote header.php with Beans markup

--- woocommerce/auth/header.php
+++ woocommerce/auth/header.php
@@ -7,9 +7,7 @@
  * @version 2.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 ?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
@@ -21,6 +19,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php wp_admin_css( 'install', true ); ?>
 	<link rel="stylesheet" href="<?php echo esc_url( str_replace( array( 'http:', 'https:' ), '', WC()->plugin_url() ) . '/assets/css/auth.css' ); ?>" type="text/css" />
 </head>
-<body class="wc-auth wp-core-ui">
-	<h1 id="wc-logo"><img src="<?php echo WC()->plugin_url(); ?>/assets/images/woocommerce_logo.png" alt="WooCommerce" /></h1>
-	<div class="wc-auth-content">
+
+<?php echo beans_open_markup( 'woo_auth_body', 'body', array( 'class' => 'wc-auth wp-core-ui' ) ); ?>
+
+	<?php echo beans_open_markup( 'woo_auth_logo_heading', 'h1', array( 'id' => 'wc-logo' ) ); ?>
+
+		<?php echo beans_selfclose_markup( 'woo_auth_logo_image', 'img', array( 'src' => WC()->plugin_url() . '/assets/images/woocommerce_logo.png', 'alt' => 'WooCommerce' ) ); ?>
+
+	<?php echo beans_close_markup( 'woo_auth_content', 'h1' ); ?>
+
+	<?php echo beans_open_markup( 'woo_auth_content', 'div', array( 'class' => 'wc-auth-content' ) ); ?>
