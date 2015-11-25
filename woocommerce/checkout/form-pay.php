2243fe34836b76acd0bc1fc80b56a472cb374e53
Chris Rault <chris@devignerforhire.com>
2015-11-25 11:30:43 +0200

    Rewrote form-pay.php with Beans markup

--- woocommerce/checkout/form-pay.php
+++ woocommerce/checkout/form-pay.php
@@ -7,93 +7,181 @@
  * @version  2.4.7
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 ?>
-<form id="order_review" method="post">
+<?php echo beans_open_markup( 'woo_checkout_pay_form', 'form', array( 'id' => 'order_review', 'method' => 'post' ) ); ?>
 
-	<table class="shop_table">
-		<thead>
-			<tr>
-				<th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
-				<th class="product-quantity"><?php _e( 'Qty', 'woocommerce' ); ?></th>
-				<th class="product-total"><?php _e( 'Totals', 'woocommerce' ); ?></th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-			if ( sizeof( $order->get_items() ) > 0 ) :
-				foreach ( $order->get_items() as $item ) :
-					echo '
-						<tr>
-							<td class="product-name">' . $item['name'].'</td>
-							<td class="product-quantity">' . $item['qty'].'</td>
-							<td class="product-subtotal">' . $order->get_formatted_line_subtotal( $item ) . '</td>
-						</tr>';
-				endforeach;
-			endif;
-			?>
-		</tbody>
-		<tfoot>
-		<?php
-			if ( $totals = $order->get_order_item_totals() ) foreach ( $totals as $total ) :
-				?>
-				<tr>
-					<th scope="row" colspan="2"><?php echo $total['label']; ?></th>
-					<td class="product-total"><?php echo $total['value']; ?></td>
-				</tr>
-				<?php
-			endforeach;
-		?>
-		</tfoot>
-	</table>
+	<?php echo beans_open_markup( 'woo_checkout_pay_table', 'table', array( 'class' => 'shop_table' ) ); ?>
 
-	<div id="payment">
+		<?php echo beans_open_markup( 'woo_checkout_pay_thead', 'thead' ); ?>
+
+			<?php echo beans_open_markup( 'woo_checkout_pay_thead_row', 'tr' ); ?>
+
+				<?php echo beans_open_markup( 'woo_checkout_pay_thead_name', 'th', array( 'class' => 'product-name' ) ); ?>
+
+					<?php _e( 'Product', 'woocommerce' ); ?>
+
+				<?php echo beans_close_markup( 'woo_checkout_pay_thead_name' ) ); ?>
+
+				<?php echo beans_open_markup( 'woo_checkout_pay_thead_quantity', 'th', array( 'class' => 'product-quantity' ) ); ?>
+
+					<?php _e( 'Qty', 'woocommerce' ); ?>
+
+				<?php echo beans_close_markup( 'woo_checkout_pay_thead_quantity' ) ); ?>
+
+				<?php echo beans_open_markup( 'woo_checkout_pay_thead_total', 'th', array( 'class' => 'product-total' ) ); ?>
+
+					<?php _e( 'Totals', 'woocommerce' ); ?>
+
+				<?php echo beans_close_markup( 'woo_checkout_pay_thead_total' ) ); ?>
+
+			<?php echo beans_close_markup( 'woo_checkout_pay_thead_row', 'tr' ); ?>
+
+		<?php echo beans_close_markup( 'woo_checkout_pay_thead', 'thead' ); ?>
+
+		<?php echo beans_open_markup( 'woo_checkout_pay_tbody', 'tbody' ); ?>
+
+			<?php if ( sizeof( $order->get_items() ) > 0 ) : ?>
+
+				<?php foreach ( $order->get_items() as $item ) : ?>
+
+					<?php echo beans_open_markup( 'woo_checkout_pay_tbody_row', 'tr' ); ?>
+
+						<?php echo beans_open_markup( 'woo_checkout_pay_tbody_name', 'td', array( 'class' => 'product-name' ) ); ?>
+
+							<?php echo $item['name']; ?>
+
+						<?php echo beans_close_markup( 'woo_checkout_pay_tbody_name', 'td' ); ?>
+
+						<?php echo beans_open_markup( 'woo_checkout_pay_tbody_quantity', 'td', array( 'class' => 'product-quantity' ) ); ?>
+
+							<?php echo $item['qty']; ?>
+
+						<?php echo beans_close_markup( 'woo_checkout_pay_tbody_quantity', 'td' ); ?>
+
+						<?php echo beans_open_markup( 'woo_checkout_pay_tbody_subtotal', 'td', array( 'class' => 'product-subtotal' ) ); ?>
+
+							<?php echo $order->get_formatted_line_subtotal( $item ); ?>
+
+						<?php echo beans_close_markup( 'woo_checkout_pay_tbody_subtotal', 'td' ); ?>
+
+					<?php echo beans_close_markup( 'woo_checkout_pay_tbody_row', 'tr' ); ?>
+
+				<?php endforeach; ?>
+
+			<?php endif; ?>
+
+		<?php echo beans_close_markup( 'woo_checkout_pay_tbody', 'tbody' ); ?>
+
+		<?php echo beans_open_markup( 'woo_checkout_pay_tfoot', 'tfoot' ); ?>
+
+			<?php echo beans_open_markup( 'woo_checkout_pay_tfoot_row', 'tr' ); ?>
+
+			<?php if ( $totals = $order->get_order_item_totals() ) foreach ( $totals as $total ) : ?>
+
+				<?php echo beans_open_markup( 'woo_checkout_pay_total_label', 'th', array( 'scope' => 'row', 'colspan' => 2 ) ); ?>
+
+					<?php echo $total['label']; ?>
+
+				<?php echo beans_close_markup( 'woo_checkout_pay_total_label', 'th' ); ?>
+
+				<?php echo beans_open_markup( 'woo_checkout_pay_total_value', 'td', array( 'class' => 'product-total' ) ); ?>
+
+					<?php echo $total['value']; ?>
+
+				<?php echo beans_close_markup( 'woo_checkout_pay_total_value', 'td' ) ); ?>
+
+			<?php endforeach; ?>
+
+			<?php echo beans_close_markup( 'woo_checkout_pay_tfoot_row', 'tr' ); ?>
+
+		<?php echo beans_close_markup( 'woo_checkout_pay_tfoot', 'tfoot' ); ?>
+
+	<?php echo beans_close_markup( 'woo_checkout_pay_table', 'table' ); ?>
+
+	<?php echo beans_open_markup( 'woo_checkout_payment', 'div', array( 'id' => 'payment' ) ); ?>
+
 		<?php if ( $order->needs_payment() ) : ?>
-		<ul class="payment_methods methods">
-			<?php
-				if ( $available_gateways = WC()->payment_gateways->get_available_payment_gateways() ) {
+
+		<?php echo beans_open_markup( 'woo_checkout_payment_methods', 'ul', array( 'class' => 'payment_methods methods' ) ); ?>
+
+			<?php if ( $available_gateways = WC()->payment_gateways->get_available_payment_gateways() ) :
+
+					<?php
 					// Chosen Method
-					if ( sizeof( $available_gateways ) ) {
-						current( $available_gateways )->set_current();
-					}
+					if ( sizeof( $available_gateways ) ) : ?>
 
-					foreach ( $available_gateways as $gateway ) {
-						?>
-						<li class="payment_method_<?php echo $gateway->id; ?>">
-							<input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>" />
-							<label for="payment_method_<?php echo $gateway->id; ?>"><?php echo $gateway->get_title(); ?> <?php echo $gateway->get_icon(); ?></label>
-							<?php
-								if ( $gateway->has_fields() || $gateway->get_description() ) {
-									echo '<div class="payment_box payment_method_' . $gateway->id . '" style="display:none;">';
-									$gateway->payment_fields();
-									echo '</div>';
-								}
-							?>
-						</li>
-						<?php
-					}
-				} else {
+						<?php current( $available_gateways )->set_current(); ?>
 
-					echo '<p>' . __( 'Sorry, it seems that there are no available payment methods for your location. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) . '</p>';
+					<?php endif; ?>
 
-				}
-			?>
-		</ul>
+					<?php foreach ( $available_gateways as $gateway ) : ?>
+
+						<?php echo beans_open_markup( 'woo_checkout_payment_methods_gateway', 'li', array( 'class' => 'payment_method_' . $gateway->id ) ); ?>
+
+							<?php echo beans_selfclose_markup( 'woo_checkout_payment_methods_gateway_input', 'input', array(
+								'id' => 'payment_method_' . $gateway->id,
+								'type' => 'radio',
+								'class' => 'input-radio',
+								'name' => 'payment_method',
+								'value' => esc_attr( $gateway->id ),
+								'checked' => checked( $gateway->chosen, true ),
+								#TODO Double check
+								'data-order_button_text' => esc_attr( $gateway->order_button_text )
+								) ); ?>
+
+							<?php echo beans_open_markup( 'woo_checkout_payment_methods_gateway_label', 'label', array( 'for' => 'payment_method_' . $gateway->id ) ); ?>
+
+								<?php echo $gateway->get_title(); ?> <?php echo $gateway->get_icon(); ?>
+
+							<?php echo beans_close_markup( 'woo_checkout_payment_methods_gateway_label', 'label' ); ?>
+
+							<?php if ( $gateway->has_fields() || $gateway->get_description() ) : ?>
+
+								<?php echo beans_open_markup( 'woo_checkout_payment_method_fields', 'div', array( 'class' => 'payment_box payment_method_' . $gateway->id ) ); ?>
+
+									<?php $gateway->payment_fields(); ?>
+
+								<?php echo beans_close_markup( 'woo_checkout_payment_method_fields', 'div' ) ); ?>
+
+							<?php endif; ?>
+
+						<?php echo beans_close_markup( 'woo_checkout_payment_methods_gateway', 'li' ) ); ?>
+
+					<?php endforeach; ?>
+
+				<?php else : ?>
+
+					<?php echo beans_open_markup( 'woo_checkout_no_payment_method_notice', 'p' ) . __( 'Sorry, it seems that there are no available payment methods for your location. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) . beans_close_markup( 'woo_checkout_no_payment_method_notice', 'p' );
+
+				<?php endif; ?>
+
+		<?php echo beans_close_markup( 'woo_checkout_payment_methods', 'ul' ) ); ?>
+
 		<?php endif; ?>
 
-		<div class="form-row">
+		<?php echo beans_open_markup( 'woo_checkout_place_order', 'div', array( 'class' => 'form-row' ) ); ?>
+
 			<?php wp_nonce_field( 'woocommerce-pay' ); ?>
-			<?php
-				$pay_order_button_text = apply_filters( 'woocommerce_pay_order_button_text', __( 'Pay for order', 'woocommerce' ) );
 
-				echo apply_filters( 'woocommerce_pay_order_button_html', '<input type="submit" class="button alt" id="place_order" value="' . esc_attr( $pay_order_button_text ) . '" data-value="' . esc_attr( $pay_order_button_text ) . '" />' );
-			?>
+			<?php $pay_order_button_text = apply_filters( 'woocommerce_pay_order_button_text', __( 'Pay for order', 'woocommerce' ) ); ?>
+
+				<?php echo apply_filters( 'woocommerce_pay_order_button_html', beans_selfclose_markup( 'woo_pay_order_button_html', 'input', array(
+					'type' => 'submit',
+					'class' => 'button alt',
+					'id' => 'place_order',
+					'value' => esc_attr( $pay_order_button_text ),
+					'data-value' => esc_attr( $pay_order_button_text )
+					) ) ); ?>
+
 			<input type="hidden" name="woocommerce_pay" value="1" />
-		</div>
 
-	</div>
+		<?php echo beans_close_markup( 'woo_checkout_place_order', 'div' ) ); ?>
 
-</form>
+	<?php echo beans_close_markup( 'woo_checkout_payment', 'div' ) ); ?>
+
+<?php echo beans_close_markup( 'woo_checkout_pay_form', 'form', array(
+	'id' => 'order_review',
+	'method' => 'post'
+) ); ?>
