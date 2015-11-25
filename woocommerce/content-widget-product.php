27ee1adc5c15e5b60b9b068e2c9dbfa3d7534136
Chris Rault <chris@devignerforhire.com>
2015-11-25 14:39:27 +0200

    Updated the markup id's to be shorter

--- woocommerce/content-widget-product.php
+++ woocommerce/content-widget-product.php
@@ -15,25 +15,25 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 <?php global $product; ?>
 
-<?php echo beans_open_markup( 'woo_content_product_widget_list_item', 'li' ); ?>
+<?php echo beans_open_markup( 'woo_product_widget_list_item', 'li' ); ?>
 
-	<?php echo beans_open_markup( 'woo_content_product_widget_list_item_link', 'a', array(
+	<?php echo beans_open_markup( 'woo_product_widget_list_item_link', 'a', array(
 		'href' => esc_url( get_permalink( $product->id ) ),
 		'title' => esc_attr( $product->get_title() )
 	) ); ?>
 
 		<?php echo $product->get_image(); ?>
 
-		<?php echo beans_open_markup( 'woo_content_product_widget_list_item_title', 'span', array( 'class' => 'product-title' ) ); ?>
+		<?php echo beans_open_markup( 'woo_product_widget_list_item_title', 'span', array( 'class' => 'product-title' ) ); ?>
 
 			<?php echo $product->get_title(); ?>
 
-		<?php echo beans_close_markup( 'woo_content_product_widget_list_item_title', 'span' ); ?>
+		<?php echo beans_close_markup( 'woo_product_widget_list_item_title', 'span' ); ?>
 
-	<?php echo beans_close_markup( 'woo_content_product_widget_list_item_link', 'a' ); ?>
+	<?php echo beans_close_markup( 'woo_product_widget_list_item_link', 'a' ); ?>
 
 	<?php if ( ! empty( $show_rating ) ) echo $product->get_rating_html(); ?>
 
 	<?php echo $product->get_price_html(); ?>
 
-<?php echo beans_close_markup( 'woo_content_product_widget_list_item', 'li' ); ?>
+<?php echo beans_close_markup( 'woo_product_widget_list_item', 'li' ); ?>
