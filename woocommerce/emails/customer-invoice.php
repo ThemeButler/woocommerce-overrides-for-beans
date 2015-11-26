8eb93d717f1e9b8da19cd575d3a2898ad7893f02
Chris Rault <chris@devignerforhire.com>
2015-11-26 14:35:10 +0200

    Rewrote customer-invoice.php with Beans markup

--- woocommerce/emails/customer-invoice.php
+++ woocommerce/emails/customer-invoice.php
@@ -7,9 +7,7 @@
  * @version     2.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 ?>
 
@@ -17,23 +15,71 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <?php if ( $order->has_status( 'pending' ) ) : ?>
 
-	<p><?php printf( __( 'An order has been created for you on %s. To pay for this order please use the following link: %s', 'woocommerce' ), get_bloginfo( 'name', 'display' ), '<a href="' . esc_url( $order->get_checkout_payment_url() ) . '">' . __( 'pay', 'woocommerce' ) . '</a>' ); ?></p>
+	<?php echo beans_open_markup( 'woo_emails_customer_invoice_notice', 'p' ); ?>
+
+		<?php printf( __( 'An order has been created for you on %s. To pay for this order please use the following link: %s', 'woocommerce' ), get_bloginfo( 'name', 'display' ), '<a href="' . esc_url( $order->get_checkout_payment_url() ) . '">' . __( 'pay', 'woocommerce' ) . '</a>' ); ?>
+
+	<?php echo beans_close_markup( 'woo_emails_customer_invoice_notice', 'p' ); ?>
 
 <?php endif; ?>
 
 <?php do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plain_text ); ?>
 
-<h2><?php printf( __( 'Order #%s', 'woocommerce' ), $order->get_order_number() ); ?> (<?php printf( '<time datetime="%s">%s</time>', date_i18n( 'c', strtotime( $order->order_date ) ), date_i18n( wc_date_format(), strtotime( $order->order_date ) ) ); ?>)</h2>
+<?php echo beans_open_markup( 'woo_emails_customer_invoice_title', 'h2' ); ?>
 
-<table class="td" cellspacing="0" cellpadding="6" style="width: 100%; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" border="1">
-	<thead>
-		<tr>
-			<th class="td" scope="col" style="text-align:left;"><?php _e( 'Product', 'woocommerce' ); ?></th>
-			<th class="td" scope="col" style="text-align:left;"><?php _e( 'Quantity', 'woocommerce' ); ?></th>
-			<th class="td" scope="col" style="text-align:left;"><?php _e( 'Price', 'woocommerce' ); ?></th>
-		</tr>
-	</thead>
-	<tbody>
+	<?php printf( __( 'Order #%s', 'woocommerce' ), $order->get_order_number() ); ?> (<?php printf( beans_open_markup( 'woo_emails_customer_invoice_time', 'time', array( 'datetime' => '%s' ) ) . '%s' . beans_close_markup( 'woo_emails_customer_invoice_time', 'time', date_i18n( 'c', strtotime( $order->order_date ) ), date_i18n( wc_date_format(), strtotime( $order->order_date ) ) ); ?>)
+
+<?php echo beans_close_markup( 'woo_emails_customer_invoice_title', 'h2' ); ?>
+
+<?php echo beans_open_markup( 'woo_emails_customer_invoice_table', 'table', array(
+	'class' => 'td',
+	'cellspacing' => 0,
+	'cellpadding' => 6,
+	'style' => 'width: 100%; font-family: \'Helvetica Neue\', Helvetica, Roboto, Arial, sans-serif;',
+	'border' => 0
+) );
+?>
+
+<?php echo beans_open_markup( 'woo_emails_customer_invoice_table_head', 'thead' ); ?>
+
+	<?php echo beans_open_markup( 'woo_emails_customer_invoice_table_heading_row', 'tr' ); ?>
+
+		<?php echo beans_open_markup( 'woo_emails_customer_invoice_table_heading_product', 'th', array(
+			'class' => 'td',
+			'scope' => 'col',
+			'style' => 'text-align:left;'
+		) ); ?>
+
+			<?php _e( 'Product', 'woocommerce' ); ?>
+
+		<?php echo beans_close_markup( 'woo_emails_customer_invoice_table_heading_product', 'th' ); ?>
+
+		<?php echo beans_open_markup( 'woo_emails_customer_invoice_table_heading_quantity', 'th', array(
+			'class' => 'td',
+			'scope' => 'col',
+			'style' => 'text-align:left;'
+		) ); ?>
+
+			<?php _e( 'Quantity', 'woocommerce' ); ?>
+
+		<?php echo beans_close_markup( 'woo_emails_customer_invoice_table_heading_quantity', 'th' ); ?>
+
+		<?php echo beans_open_markup( 'woo_emails_customer_invoice_table_heading_price', 'th', array(
+			'class' => 'td',
+			'scope' => 'col',
+			'style' => 'text-align:left;'
+		) ); ?>
+
+			<?php _e( 'Price', 'woocommerce' ); ?>
+
+		<?php echo beans_close_markup( 'woo_emails_customer_invoice_table_heading_price', 'th' ); ?>
+
+	<?php echo beans_close_markup( 'woo_emails_customer_invoice_table_heading_row', 'tr' ); ?>
+
+<?php echo beans_close_markup( 'woo_emails_customer_invoice_table_head', 'thead' ); ?>
+
+<?php echo beans_open_markup( 'woo_emails_customer_invoice_table_body', 'tbody' ); ?>
+
 		<?php
 			switch ( $order->get_status() ) {
 				case "completed" :
@@ -47,22 +93,52 @@ if ( ! defined( 'ABSPATH' ) ) {
 				break;
 			}
 		?>
-	</tbody>
-	<tfoot>
+
+	<?php echo beans_close_markup( 'woo_emails_customer_invoice_table_body', 'tbody' ); ?>
+
+	<?php echo beans_open_markup( 'woo_emails_customer_invoice_table_foot', 'tfoot' ); ?>
+
 		<?php
 			if ( $totals = $order->get_order_item_totals() ) {
 				$i = 0;
 				foreach ( $totals as $total ) {
 					$i++;
-					?><tr>
-						<td class="td" colspan="2" style="text-align:left; <?php if ( $i == 1 ) echo 'border-top-width: 4px !important;'; ?>"><?php echo $total['label']; ?></td>
-						<td class="td" style="text-align:left; <?php if ( $i == 1 ) echo 'border-top-width: 4px !important;'; ?>"><?php echo $total['value']; ?></td>
-					</tr><?php
+					?>
+
+					<?php echo beans_open_markup( 'woo_emails_customer_invoice_table_row', 'tr' ); ?>
+
+						<?php echo beans_open_markup( 'woo_emails_customer_invoice_table_total_label', 'th', array(
+							'class' => 'td',
+							'scope' => 'row',
+							'colspan' => 2,
+							'style' => 'text-align: left;' . $i == 1 ? ' border-top-width: 4px;' : null
+							#TODO Double check
+						) ); ?>
+
+							<?php echo $total['label']; ?>
+
+						<?php echo beans_open_markup( 'woo_emails_customer_invoice_table_total_label', 'th' ); ?>
+
+						<?php echo beans_open_markup( 'woo_emails_customer_invoice_table_total_value', 'td', array(
+							'class' => 'td',
+							'style' => 'text-align: left;' . $i == 1 ? ' border-top-width: 4px;' : null
+							#TODO Double check
+						) ); ?>
+
+							<?php echo $total['value']; ?>
+
+						<?php echo beans_open_markup( 'woo_emails_customer_invoice_table_total_value', 'td' ); ?>
+
+					<?php echo beans_close_markup( 'woo_emails_customer_invoice_table_row', 'tr' ); ?>
+
+					<?php
 				}
 			}
 		?>
-	</tfoot>
-</table>
+
+		<?php echo beans_close_markup( 'woo_emails_customer_invoice_table_foot', 'tfoot' ); ?>
+
+	<?php echo beans_close_markup( 'woo_emails_customer_invoice_table', 'table' ); ?>
 
 <?php do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text ); ?>
 
