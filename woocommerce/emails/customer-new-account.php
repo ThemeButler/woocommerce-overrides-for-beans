9f273a04a9ec96f16b04ade41572b7a62c95e94d
Chris Rault <chris@devignerforhire.com>
2015-11-26 14:35:24 +0200

    Rewrote customer-new-account.php with Beans markup

--- woocommerce/emails/customer-new-account.php
+++ woocommerce/emails/customer-new-account.php
@@ -15,14 +15,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <?php do_action( 'woocommerce_email_header', $email_heading ); ?>
 
-<p><?php printf( __( "Thanks for creating an account on %s. Your username is <strong>%s</strong>.", 'woocommerce' ), esc_html( $blogname ), esc_html( $user_login ) ); ?></p>
+<?php echo beans_open_markup( 'woo_emails_customer_new_account_notice', 'p' ); ?>
+
+	<?php printf( __( "Thanks for creating an account on %s. Your username is <strong>%s</strong>.", 'woocommerce' ), esc_html( $blogname ), esc_html( $user_login ) ); ?>
+
+<?php echo beans_close_markup( 'woo_emails_customer_new_account_notice', 'p' ); ?>
 
 <?php if ( get_option( 'woocommerce_registration_generate_password' ) == 'yes' && $password_generated ) : ?>
 
-	<p><?php printf( __( "Your password has been automatically generated: <strong>%s</strong>", 'woocommerce' ), esc_html( $user_pass ) ); ?></p>
+	<?php echo beans_open_markup( 'woo_emails_customer_generated_password_notice', 'p' ); ?>
+
+		<?php printf( __( "Your password has been automatically generated: <strong>%s</strong>", 'woocommerce' ), esc_html( $user_pass ) ); ?>
+
+	<?php echo beans_close_markup( 'woo_emails_customer_generated_password_notice', 'p' ); ?>
 
 <?php endif; ?>
 
-<p><?php printf( __( 'You can access your account area to view your orders and change your password here: %s.', 'woocommerce' ), wc_get_page_permalink( 'myaccount' ) ); ?></p>
+<?php echo beans_open_markup( 'woo_emails_customer_account_link_notice', 'p' ); ?>
+
+	<?php printf( __( 'You can access your account area to view your orders and change your password here: %s.', 'woocommerce' ), wc_get_page_permalink( 'myaccount' ) ); ?></p>
+
+<?php echo beans_close_markup( 'woo_emails_customer_account_link_notice', 'p' ); ?>
 
 <?php do_action( 'woocommerce_email_footer' ); ?>
