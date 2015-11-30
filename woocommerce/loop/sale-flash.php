0912c014beee3523d6569c55f55f4c035e8bf1eb
Chris Rault <chris@devignerforhire.com>
2015-11-30 11:43:28 +0200

    Rewrote sale-flash.php with Beans markup

--- woocommerce/loop/sale-flash.php
+++ woocommerce/loop/sale-flash.php
@@ -7,15 +7,12 @@
  * @version     1.6.4
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $post, $product;
 
-?>
-<?php if ( $product->is_on_sale() ) : ?>
+if ( $product->is_on_sale() ) :
 
-	<?php echo apply_filters( 'woocommerce_sale_flash', '<span class="onsale">' . __( 'Sale!', 'woocommerce' ) . '</span>', $post, $product ); ?>
+	echo apply_filters( 'woocommerce_sale_flash', beans_open_markup( 'woo_sale_flash_notice', 'span', array( 'class' => 'onsale' ) ) . __( 'Sale!', 'woocommerce' ) . beans_open_markup( 'woo_sale_flash_notice', 'span' ), $post, $product );
 
-<?php endif; ?>
+endif;
