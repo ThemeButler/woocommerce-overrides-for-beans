a926734e3bce18e83bef0d857dbac17c4dc9973d
Chris Rault <chris@devignerforhire.com>
2015-11-30 11:41:32 +0200

    Rewrote result-count.php with Beans markup

--- woocommerce/loop/result-count.php
+++ woocommerce/loop/result-count.php
@@ -9,29 +9,33 @@
  * @version     2.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $wp_query;
 
 if ( ! woocommerce_products_will_display() )
 	return;
-?>
-<p class="woocommerce-result-count">
-	<?php
+
+echo beans_open_markup( 'woo_results_count', 'p', array( 'class' => 'woocommerce-result-count' ) );
+
 	$paged    = max( 1, $wp_query->get( 'paged' ) );
 	$per_page = $wp_query->get( 'posts_per_page' );
 	$total    = $wp_query->found_posts;
 	$first    = ( $per_page * $paged ) - $per_page + 1;
 	$last     = min( $total, $wp_query->get( 'posts_per_page' ) * $paged );
 
-	if ( 1 == $total ) {
+	if ( 1 == $total ) :
+
 		_e( 'Showing the single result', 'woocommerce' );
-	} elseif ( $total <= $per_page || -1 == $per_page ) {
+
+	elseif ( $total <= $per_page || -1 == $per_page ) :
+
 		printf( __( 'Showing all %d results', 'woocommerce' ), $total );
-	} else {
+
+	else :
+
 		printf( _x( 'Showing %1$d&ndash;%2$d of %3$d results', '%1$d = first, %2$d = last, %3$d = total', 'woocommerce' ), $first, $last, $total );
-	}
-	?>
-</p>
+
+	endif;
+
+echo beans_close_markup( 'woo_results_count', 'p' );
