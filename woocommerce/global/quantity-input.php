2f834a86df63dd0e3691679be7c37fc67cf4e916
Chris Rault <chris@devignerforhire.com>
2015-11-30 11:13:09 +0200

    Rewrote quantity-input.php with Beans markup

--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -7,9 +7,21 @@
  * @version     2.1.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-?>
-<div class="quantity"><input type="number" step="<?php echo esc_attr( $step ); ?>" <?php if ( is_numeric( $min_value ) ) : ?>min="<?php echo esc_attr( $min_value ); ?>"<?php endif; ?> <?php if ( is_numeric( $max_value ) ) : ?>max="<?php echo esc_attr( $max_value ); ?>"<?php endif; ?> name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" class="input-text qty text" size="4" /></div>
+echo beans_open_markup( 'woo_quantity_input_wrap', 'div', array( 'class' => 'quantity' ) );
+
+    echo beans_selfclose_markup( 'woo_quantity_input', 'input', array(
+        'type' => 'number',
+        'step' => esc_attr( $step ),
+        'min' => is_numeric( $min_value ) ? esc_attr( $min_value ) : null,
+        'max' => is_numeric( $max_value ) ? esc_attr( $max_value ) : null,
+        #TODO Double check
+        'name' => esc_attr( $input_name ),
+        'value' => esc_attr( $input_value ),
+        'title' => esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ),
+        'class' => 'input-text qty text',
+        'size' => 4
+     ) );
+
+echo beans_close_markup( 'woo_quantity_input_wrap', 'div' );
