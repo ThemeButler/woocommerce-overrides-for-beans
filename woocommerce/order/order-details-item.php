f53d4aab62bd166ab2bd5f2adc07f6467f837f26
Chris Rault <chris@devignerforhire.com>
2015-11-30 13:06:11 +0200

    Rewrote order-details-item.php with Beans markup

--- woocommerce/order/order-details-item.php
+++ woocommerce/order/order-details-item.php
@@ -7,36 +7,52 @@
  * @version 2.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
+if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) :
+
 	return;
-}
-?>
-<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_order_item_class', 'order_item', $item, $order ) ); ?>">
-	<td class="product-name">
-		<?php
-			$is_visible = $product && $product->is_visible();
 
-			echo apply_filters( 'woocommerce_order_item_name', $is_visible ? sprintf( '<a href="%s">%s</a>', get_permalink( $item['product_id'] ), $item['name'] ) : $item['name'], $item, $is_visible );
-			echo apply_filters( 'woocommerce_order_item_quantity_html', ' <strong class="product-quantity">' . sprintf( '&times; %s', $item['qty'] ) . '</strong>', $item );
+endif;
+
+echo beans_open_markup( 'woo_order_details_row', 'tr', array( 'class' => esc_attr( apply_filters( 'woocommerce_order_item_class', 'order_item', $item, $order ) ) ) );
+
+	echo beans_open_markup( 'woo_order_details_product_name', 'td', array( 'class' => 'product-name' ) );
+
+		$is_visible = $product && $product->is_visible();
+
+			echo apply_filters( 'woocommerce_order_item_name', $is_visible ? sprintf( beans_open_markup( 'woo_order_details_product_name_link', 'a', array( 'href' => '%s' ) ) . '%s' . beans_close_markup( 'woo_order_details_product_name_link', 'a' ), get_permalink( $item['product_id'] ), $item['name'] ) : $item['name'], $item, $is_visible );
+
+			echo apply_filters( 'woocommerce_order_item_quantity_html', ' ' . beans_open_markup( 'woo_order_details_product_quantity', 'strong', array( 'class' => 'product-quantity' ) ) . sprintf( '&times; %s', $item['qty'] ) . beans_close_markup( 'woo_order_details_product_quantity', 'strong' ), $item );
 
 			do_action( 'woocommerce_order_item_meta_start', $item_id, $item, $order );
 
 			$order->display_item_meta( $item );
+
 			$order->display_item_downloads( $item );
 
 			do_action( 'woocommerce_order_item_meta_end', $item_id, $item, $order );
-		?>
-	</td>
-	<td class="product-total">
-		<?php echo $order->get_formatted_line_subtotal( $item ); ?>
-	</td>
-</tr>
-<?php if ( $order->has_status( array( 'completed', 'processing' ) ) && ( $purchase_note = get_post_meta( $product->id, '_purchase_note', true ) ) ) : ?>
-<tr class="product-purchase-note">
-	<td colspan="3"><?php echo wpautop( do_shortcode( wp_kses_post( $purchase_note ) ) ); ?></td>
-</tr>
-<?php endif; ?>
+
+	echo beans_close_markup( 'woo_order_details_product_name', 'td' );
+
+	echo beans_open_markup( 'woo_order_details_product_total', 'td', array( 'class' => 'product-total' ) );
+
+		echo $order->get_formatted_line_subtotal( $item );
+
+	echo beans_close_markup( 'woo_order_details_product_total', 'td' );
+
+echo beans_close_markup( 'woo_order_details_row', 'tr' );
+
+if ( $order->has_status( array( 'completed', 'processing' ) ) && ( $purchase_note = get_post_meta( $product->id, '_purchase_note', true ) ) ) :
+
+	echo beans_open_markup( 'woo_order_purchase_note', 'tr', array( 'class' => 'product-purchase-note' ) );
+
+		echo beans_open_markup( 'woo_order_purchase_note_cell', 'td', array( 'colspan' => 3 ) );
+
+			echo wpautop( do_shortcode( wp_kses_post( $purchase_note ) ) );
+
+		echo beans_close_markup( 'woo_order_purchase_note_cell', 'td' );
+
+	echo beans_close_markup( 'woo_order_purchase_note', 'tr' );
+
+endif;
