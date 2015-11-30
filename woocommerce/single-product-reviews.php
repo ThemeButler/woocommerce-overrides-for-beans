2f63fe17533f8c010c4b60274c9b0a1de40110c2
Chris Rault <chris@devignerforhire.com>
2015-11-30 13:19:14 +0200

    Updated the php block formatting

--- woocommerce/single-product-reviews.php
+++ woocommerce/single-product-reviews.php
@@ -16,14 +16,13 @@ if ( ! comments_open() ) :
 
 endif;
 
-?>
-<?php echo beans_open_markup( 'woo_single_reviews_wrap', 'div', array( 'id' => 'reviews' ) ); ?>
+echo beans_open_markup( 'woo_single_reviews_wrap', 'div', array( 'id' => 'reviews' ) );
 
-	<?php echo beans_open_markup( 'woo_single_reviews_comments_wrap', 'div', array( 'id' => 'comments' ) ); ?>
+	echo beans_open_markup( 'woo_single_reviews_comments_wrap', 'div', array( 'id' => 'comments' ) );
 
-		<?php echo beans_open_markup( 'woo_single_reviews_title', 'h2' ); ?>
+		echo beans_open_markup( 'woo_single_reviews_title', 'h2' );
 
-			<?php if ( get_option( 'woocommerce_enable_review_rating' ) === 'yes' && ( $count = $product->get_review_count() ) )
+			if ( get_option( 'woocommerce_enable_review_rating' ) === 'yes' && ( $count = $product->get_review_count() ) )
 
 					printf( _n( '%s review for %s', '%s reviews for %s', $count, 'woocommerce' ), $count, get_the_title() );
 
@@ -31,52 +30,52 @@ endif;
 
 					_e( 'Reviews', 'woocommerce' );
 
-				endif; ?>
+				endif;
 
-		<?php echo beans_close_markup( 'woo_single_reviews_title', 'h2' ); ?>
+		echo beans_close_markup( 'woo_single_reviews_title', 'h2' );
 
-		<?php if ( have_comments() ) : ?>
+		if ( have_comments() ) :
 
-			<?php echo beans_open_markup( 'woo_single_reviews_comment_list', 'ol', array( 'class' => 'commentlist' ) ); ?>
+			echo beans_open_markup( 'woo_single_reviews_comment_list', 'ol', array( 'class' => 'commentlist' ) );
 
-				<?php wp_list_comments( apply_filters( 'woocommerce_product_review_list_args', array( 'callback' => 'woocommerce_comments' ) ) ); ?>
+				wp_list_comments( apply_filters( 'woocommerce_product_review_list_args', array( 'callback' => 'woocommerce_comments' ) ) );
 
-			<?php echo beans_close_markup( 'woo_single_reviews_comment_list', 'ol' ); ?>
+			echo beans_close_markup( 'woo_single_reviews_comment_list', 'ol' );
 
-			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+			if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
 
-				<?php echo beans_open_markup( 'woo_single_reviews_pagination', 'nav', array( 'class' => 'woocommerce-pagination' ) ); ?>
+				echo beans_open_markup( 'woo_single_reviews_pagination', 'nav', array( 'class' => 'woocommerce-pagination' ) );
 
-					<?php paginate_comments_links( apply_filters( 'woocommerce_comment_pagination_args', array(
+					paginate_comments_links( apply_filters( 'woocommerce_comment_pagination_args', array(
 						'prev_text' => '&larr;',
 						'next_text' => '&rarr;',
 						'type'      => 'list',
-					) ) ); ?>
+					) ) );
 
-				<?php echo beans_close_markup( 'woo_single_reviews_pagination', 'nav' ); ?>
+				echo beans_close_markup( 'woo_single_reviews_pagination', 'nav' );
 
-			<?php endif; ?>
+			endif;
 
-		<?php else : ?>
+		else :
 
-			<?php echo beans_open_markup( 'woo_single_reviews_no_reviews', 'p', array( 'class' => 'woocommerce-noreviews' ) ); ?>
+			echo beans_open_markup( 'woo_single_reviews_no_reviews', 'p', array( 'class' => 'woocommerce-noreviews' ) );
 
-				<?php _e( 'There are no reviews yet.', 'woocommerce' ); ?>
+				_e( 'There are no reviews yet.', 'woocommerce' );
 
-			<?php echo beans_close_markup( 'woo_single_reviews_no_reviews', 'p' ); ?>
+			echo beans_close_markup( 'woo_single_reviews_no_reviews', 'p' );
 
-		<?php endif; ?>
+		endif;
 
-	<?php echo beans_close_markup( 'woo_single_reviews_comments_wrap', 'div' ); ?>
+	echo beans_close_markup( 'woo_single_reviews_comments_wrap', 'div' );
 
 
-	<?php if ( get_option( 'woocommerce_review_rating_verification_required' ) === 'no' || wc_customer_bought_product( '', get_current_user_id(), $product->id ) ) : ?>
+	if ( get_option( 'woocommerce_review_rating_verification_required' ) === 'no' || wc_customer_bought_product( '', get_current_user_id(), $product->id ) ) :
 
-		<?php echo beans_open_markup( 'woo_single_reviews_form_wrap', 'div', array( 'id' => 'review_form_wrapper' ) ); ?>
+		echo beans_open_markup( 'woo_single_reviews_form_wrap', 'div', array( 'id' => 'review_form_wrapper' ) );
 
-			<?php echo beans_open_markup( 'woo_single_reviews_form', 'div', array( 'id' => 'review_form' ) ); ?>
+			echo beans_open_markup( 'woo_single_reviews_form', 'div', array( 'id' => 'review_form' ) );
 
-				<?php $commenter = wp_get_current_commenter();
+				$commenter = wp_get_current_commenter();
 					#TODO Review
 					$comment_form = array(
 						'title_reply'          => have_comments() ? __( 'Add a review', 'woocommerce' ) : __( 'Be the first to review', 'woocommerce' ) . ' &ldquo;' . get_the_title() . '&rdquo;',
@@ -109,7 +108,7 @@ endif;
 
 					if ( $account_page_url = wc_get_page_permalink( 'myaccount' ) ) :
 
-						$comment_form['must_log_in'] = '<p class="must-log-in">' .  sprintf( __( 'You must be <a href="%s">logged in</a> to post a review.', 'woocommerce' ), esc_url( $account_page_url ) ) . '</p>';
+						$comment_form['must_log_in'] = '<p class="must-log-in">' .  sprintf( __( 'You must be ' . beans_open_markup( 'woo_single_reviews_form_login_link', 'a', array( 'href' => '%s' ) ) . 'logged in' . beans_close_markup( 'woo_single_reviews_form_login_link', 'a' ) . ' to post a review.', 'woocommerce' ), esc_url( $account_page_url ) ) . '</p>';
 
 					endif;
 
@@ -117,14 +116,12 @@ endif;
 
 						$comment_form['comment_field'] = beans_open_markup( 'woo_single_reviews_form_rating', 'p', array( 'class' => 'comment-form-rating' ) ) . beans_open_markup( 'woo_single_reviews_form_rating_label', 'label', array( 'for' => 'rating' ) ) . __( 'Your Rating', 'woocommerce' ) . beans_close_markup( 'woo_single_reviews_form_rating_label', 'label' ) . beans_open_markup( 'woo_single_reviews_form_rating_select', 'select', array(
 							'name' => 'rating',
-							'id' => 'rating' ) ) ?>
-							<option value="">' . __( 'Rate&hellip;', 'woocommerce' ) . '</option>
+							'id' => 'rating' ) ) . '<option value="">' . __( 'Rate&hellip;', 'woocommerce' ) . '</option>
 							<option value="5">' . __( 'Perfect', 'woocommerce' ) . '</option>
 							<option value="4">' . __( 'Good', 'woocommerce' ) . '</option>
 							<option value="3">' . __( 'Average', 'woocommerce' ) . '</option>
 							<option value="2">' . __( 'Not that bad', 'woocommerce' ) . '</option>
-							<option value="1">' . __( 'Very Poor', 'woocommerce' ) . '</option>
-						<?php beans_close_markup( 'woo_single_reviews_form_rating_select', 'select') . beans_close_markup( 'woo_single_reviews_form_rating', 'p');
+							<option value="1">' . __( 'Very Poor', 'woocommerce' ) . '</option>' .	beans_close_markup( 'woo_single_reviews_form_rating_select', 'select') . beans_close_markup( 'woo_single_reviews_form_rating', 'p');
 
 					endif;
 
@@ -136,26 +133,26 @@ endif;
 						'aria-required' => true
 					) ) . beans_close_markup( 'woo_single_reviews_form_rating_textarea', 'textarea') . beans_close_markup( 'woo_single_reviews_form_comment', 'p');
 
-					comment_form( apply_filters( 'woocommerce_product_review_comment_form_args', $comment_form ) ); ?>
+					comment_form( apply_filters( 'woocommerce_product_review_comment_form_args', $comment_form ) );
 
-				<?php echo beans_close_markup( 'woo_single_reviews_form', 'div' ); ?>
+				echo beans_close_markup( 'woo_single_reviews_form', 'div' );
 
-			<?php echo beans_close_markup( 'woo_single_reviews_form_wrap', 'div' ); ?>
+			echo beans_close_markup( 'woo_single_reviews_form_wrap', 'div' );
 
-	<?php else : ?>
+	else :
 
-		<?php echo beans_open_markup( 'woo_single_reviews_verification_required', 'p', array( 'class' => 'woocommerce-verification-required' ) ); ?>
+		echo beans_open_markup( 'woo_single_reviews_verification_required', 'p', array( 'class' => 'woocommerce-verification-required' ) );
 
-			<?php _e( 'Only logged in customers who have purchased this product may leave a review.', 'woocommerce' ); ?>
+			_e( 'Only logged in customers who have purchased this product may leave a review.', 'woocommerce' );
 
-		<?php echo beans_close_markup( 'woo_single_reviews_verification_required', 'p' ); ?>
+		echo beans_close_markup( 'woo_single_reviews_verification_required', 'p' );
 
-	<?php endif; ?>
+	endif;
 
-	<?php echo beans_open_markup( 'woo_single_reviews_clear', 'div', array( 'class' => 'clear' ) ); ?>
+	echo beans_open_markup( 'woo_single_reviews_clear', 'div', array( 'class' => 'clear' ) );
 
-		<?php _e( 'Only logged in customers who have purchased this product may leave a review.', 'woocommerce' ); ?>
+		_e( 'Only logged in customers who have purchased this product may leave a review.', 'woocommerce' );
 
-	<?php echo beans_close_markup( 'woo_single_reviews_clear', 'div' ); ?>
+	echo beans_close_markup( 'woo_single_reviews_clear', 'div' );
 
-<?php echo beans_close_markup( 'woo_single_reviews_wrap', 'div' ); ?>
+echo beans_close_markup( 'woo_single_reviews_wrap', 'div' );
