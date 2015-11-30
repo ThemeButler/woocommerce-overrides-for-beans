413687639da1dfe3d96eec4c85064a9cb7452af0
Chris Rault <chris@devignerforhire.com>
2015-11-30 14:03:04 +0200

    Rewrote product-attributes.php with Beans markup

--- woocommerce/single-product/product-attributes.php
+++ woocommerce/single-product/product-attributes.php
@@ -9,67 +9,118 @@
  * @version     2.1.3
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-$has_row    = false;
-$alt        = 1;
+$has_row = false;
+$alt = 1;
 $attributes = $product->get_attributes();
 
 ob_start();
 
-?>
-<table class="shop_attributes">
+echo beans_open_markup( 'woo_single_attributes_table', 'table', array( 'class' => 'shop_attributes' ) );
 
-	<?php if ( $product->enable_dimensions_display() ) : ?>
+	if ( $product->enable_dimensions_display() ) :
 
-		<?php if ( $product->has_weight() ) : $has_row = true; ?>
-			<tr class="<?php if ( ( $alt = $alt * -1 ) == 1 ) echo 'alt'; ?>">
-				<th><?php _e( 'Weight', 'woocommerce' ) ?></th>
-				<td class="product_weight"><?php echo $product->get_weight() . ' ' . esc_attr( get_option( 'woocommerce_weight_unit' ) ); ?></td>
-			</tr>
-		<?php endif; ?>
+		if ( $product->has_weight() ) : $has_row = true;
 
-		<?php if ( $product->has_dimensions() ) : $has_row = true; ?>
-			<tr class="<?php if ( ( $alt = $alt * -1 ) == 1 ) echo 'alt'; ?>">
-				<th><?php _e( 'Dimensions', 'woocommerce' ) ?></th>
-				<td class="product_dimensions"><?php echo $product->get_dimensions(); ?></td>
-			</tr>
-		<?php endif; ?>
+			echo beans_open_markup( 'woo_single_attributes_weight_row', 'tr', array(
+				if ( ( $alt = $alt * -1 ) == 1 )
+				'class' => 'alt'
+			) );
 
-	<?php endif; ?>
+				echo beans_open_markup( 'woo_single_attributes_weight_heading', 'th' );
 
-	<?php foreach ( $attributes as $attribute ) :
-		if ( empty( $attribute['is_visible'] ) || ( $attribute['is_taxonomy'] && ! taxonomy_exists( $attribute['name'] ) ) ) {
+					_e( 'Weight', 'woocommerce' )
+
+				echo beans_close_markup( 'woo_single_attributes_weight_heading', 'th' );
+
+				echo beans_open_markup( 'woo_single_attributes_weight_value', 'td', array( 'class' => 'product_height' ) );
+
+					echo $product->get_weight() . ' ' . esc_attr( get_option( 'woocommerce_weight_unit' ) );
+
+				echo beans_close_markup( 'woo_single_attributes_weight_value', 'td' );
+
+			echo beans_close_markup( 'woo_single_attributes_weight_row', 'tr' );
+
+		endif;
+
+		if ( $product->has_dimensions() ) : $has_row = true;
+
+			echo beans_open_markup( 'woo_single_attributes_dimensions_row', 'tr', array(
+				if ( ( $alt = $alt * -1 ) == 1 )
+				'class' => 'alt'
+			) );
+
+				echo beans_open_markup( 'woo_single_attributes_dimensions_heading', 'th' );
+
+					_e( 'Dimensions', 'woocommerce' )
+
+				echo beans_close_markup( 'woo_single_attributes_dimensions_heading', 'th' );
+
+				echo beans_open_markup( 'woo_single_attributes_dimensions_value', 'td', array( 'class' => 'product_dimensions' ) );
+
+					echo $product->get_dimensions();
+
+				echo beans_close_markup( 'woo_single_attributes_dimensions_value', 'td' );
+
+			echo beans_close_markup( 'woo_single_attributes_weight_row', 'tr' );
+
+		endif;
+
+	endif;
+
+	foreach ( $attributes as $attribute ) :
+
+		if ( empty( $attribute['is_visible'] ) || ( $attribute['is_taxonomy'] && ! taxonomy_exists( $attribute['name'] ) ) ) :
+
 			continue;
-		} else {
+
+		else :
+
 			$has_row = true;
-		}
-		?>
-		<tr class="<?php if ( ( $alt = $alt * -1 ) == 1 ) echo 'alt'; ?>">
-			<th><?php echo wc_attribute_label( $attribute['name'] ); ?></th>
-			<td><?php
-				if ( $attribute['is_taxonomy'] ) {
+
+		endif;
+
+	echo beans_open_markup( 'woo_single_attributes_terms_row', 'tr', array(
+		if ( ( $alt = $alt * -1 ) == 1 )
+		'class' => 'alt'
+	) );
+
+		echo beans_open_markup( 'woo_single_attributes_terms_heading', 'th' );
+
+			echo wc_attribute_label( $attribute['name'] );
+
+		echo beans_close_markup( 'woo_single_attributes_terms_heading', 'th' );
+
+		echo beans_open_markup( 'woo_single_attributes_terms_value', 'td', array( 'class' => 'product_dimensions' ) );
+
+				if ( $attribute['is_taxonomy'] ) :
 
 					$values = wc_get_product_terms( $product->id, $attribute['name'], array( 'fields' => 'names' ) );
 					echo apply_filters( 'woocommerce_attribute', wpautop( wptexturize( implode( ', ', $values ) ) ), $attribute, $values );
 
-				} else {
+				else :
 
 					// Convert pipes to commas and display values
 					$values = array_map( 'trim', explode( WC_DELIMITER, $attribute['value'] ) );
 					echo apply_filters( 'woocommerce_attribute', wpautop( wptexturize( implode( ', ', $values ) ) ), $attribute, $values );
 
-				}
-			?></td>
-		</tr>
-	<?php endforeach; ?>
+				endif;
 
-</table>
-<?php
-if ( $has_row ) {
+		echo beans_close_markup( 'woo_single_attributes_terms_value', 'td' );
+
+		echo beans_close_markup( 'woo_single_attributes_terms_row', 'tr' );
+
+	endforeach;
+
+echo beans_close_markup( 'woo_single_attributes_table', 'table' );
+
+if ( $has_row ) :
+
 	echo ob_get_clean();
-} else {
+
+else :
+
 	ob_end_clean();
-}
+
+endif;
