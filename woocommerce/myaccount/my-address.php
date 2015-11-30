ae2dc19b8289e8c7b4164e316b283fc92a305781
Chris Rault <chris@devignerforhire.com>
2015-11-30 12:28:50 +0200

    Rewrote my-address.php with Beans markup

--- woocommerce/myaccount/my-address.php
+++ woocommerce/myaccount/my-address.php
@@ -7,45 +7,72 @@
  * @version     2.2.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 $customer_id = get_current_user_id();
 
-if ( ! wc_ship_to_billing_address_only() && get_option( 'woocommerce_calc_shipping' ) !== 'no' ) {
+if ( ! wc_ship_to_billing_address_only() && get_option( 'woocommerce_calc_shipping' ) !== 'no' ) :
+
 	$page_title = apply_filters( 'woocommerce_my_account_my_address_title', __( 'My Addresses', 'woocommerce' ) );
 	$get_addresses    = apply_filters( 'woocommerce_my_account_get_addresses', array(
 		'billing' => __( 'Billing Address', 'woocommerce' ),
 		'shipping' => __( 'Shipping Address', 'woocommerce' )
 	), $customer_id );
-} else {
+
+else :
+
 	$page_title = apply_filters( 'woocommerce_my_account_my_address_title', __( 'My Address', 'woocommerce' ) );
 	$get_addresses    = apply_filters( 'woocommerce_my_account_get_addresses', array(
 		'billing' =>  __( 'Billing Address', 'woocommerce' )
 	), $customer_id );
-}
+
+endif;
 
 $col = 1;
-?>
 
-<h2><?php echo $page_title; ?></h2>
+echo beans_open_markup( 'woo_my_address_title', 'h2' );
 
-<p class="myaccount_address">
-	<?php echo apply_filters( 'woocommerce_my_account_my_address_description', __( 'The following addresses will be used on the checkout page by default.', 'woocommerce' ) ); ?>
-</p>
+	echo $page_title;
 
-<?php if ( ! wc_ship_to_billing_address_only() && get_option( 'woocommerce_calc_shipping' ) !== 'no' ) echo '<div class="col2-set addresses">'; ?>
+echo beans_close_markup( 'woo_my_address_title', 'h2' );
 
-<?php foreach ( $get_addresses as $name => $title ) : ?>
+echo beans_open_markup( 'woo_my_address_description', 'p', array( 'class' => 'myaccount_address' ) );
 
-	<div class="col-<?php echo ( ( $col = $col * -1 ) < 0 ) ? 1 : 2; ?> address">
-		<header class="title">
-			<h3><?php echo $title; ?></h3>
-			<a href="<?php echo wc_get_endpoint_url( 'edit-address', $name ); ?>" class="edit"><?php _e( 'Edit', 'woocommerce' ); ?></a>
-		</header>
-		<address>
-			<?php
+	echo apply_filters( 'woocommerce_my_account_my_address_description', __( 'The following addresses will be used on the checkout page by default.', 'woocommerce' ) );
+
+echo beans_close_markup( 'woo_my_address_description', 'p' );
+
+if ( ! wc_ship_to_billing_address_only() && get_option( 'woocommerce_calc_shipping' ) !== 'no' ) :
+
+	echo beans_open_markup( 'woo_my_address_col2_address', 'div', array( 'class' => 'col2-set addresses' ) );
+
+endif;
+
+foreach ( $get_addresses as $name => $title ) :
+
+	echo beans_open_markup( 'woo_my_address_col1_address', 'div', array( 'class' => 'col-' . ( ( $col = $col * -1 ) < 0 ) ? 1 : 2 . ' address' ) );
+
+		echo beans_open_markup( 'woo_my_address_col1_header', 'header', array( 'class' => 'title' ) );
+
+			echo beans_open_markup( 'woo_my_address_col1_title', 'h3' );
+
+				echo $title;
+
+			echo beans_close_markup( 'woo_my_address_col1_title', 'h3' );
+
+			echo beans_open_markup( 'woo_my_address_col1_edit_link', 'a', array(
+				'href' => wc_get_endpoint_url( 'edit-address', $name ),
+				'class' => 'edit'
+			) );
+
+				_e( 'Edit', 'woocommerce' );
+
+			echo beans_close_markup( 'woo_my_address_col1_edit_link', 'a' );
+
+		echo beans_close_markup( 'woo_my_address_col1_header', 'header' );
+
+		echo beans_open_markup( 'woo_my_address_col1_address_wrap', 'address' );
+
 				$address = apply_filters( 'woocommerce_my_account_my_address_formatted_address', array(
 					'first_name'  => get_user_meta( $customer_id, $name . '_first_name', true ),
 					'last_name'   => get_user_meta( $customer_id, $name . '_last_name', true ),
@@ -60,14 +87,24 @@ $col = 1;
 
 				$formatted_address = WC()->countries->get_formatted_address( $address );
 
-				if ( ! $formatted_address )
+				if ( ! $formatted_address ) :
+
 					_e( 'You have not set up this type of address yet.', 'woocommerce' );
-				else
+
+				else :
+
 					echo $formatted_address;
-			?>
-		</address>
-	</div>
+					
+				endif;
 
-<?php endforeach; ?>
+		echo beans_close_markup( 'woo_my_address_col1_address_wrap', 'address' );
 
-<?php if ( ! wc_ship_to_billing_address_only() && get_option( 'woocommerce_calc_shipping' ) !== 'no' ) echo '</div>'; ?>
+	echo beans_close_markup( 'woo_my_address_col1_address', 'div' );
+
+endforeach;
+
+if ( ! wc_ship_to_billing_address_only() && get_option( 'woocommerce_calc_shipping' ) !== 'no' ) :
+
+	echo beans_close_markup( 'woo_my_address_col2_address', 'div' );
+
+endif;
