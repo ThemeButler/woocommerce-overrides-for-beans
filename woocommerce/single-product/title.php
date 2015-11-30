c38d6545e08d8b7b1b62ee3de4a59dfa793aebce
Chris Rault <chris@devignerforhire.com>
2015-11-30 14:33:17 +0200

    Rewrote title.php with Beans markup

--- woocommerce/single-product/title.php
+++ woocommerce/single-product/title.php
@@ -7,9 +7,13 @@
  * @version 1.6.4
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-?>
-<h1 itemprop="name" class="product_title entry-title"><?php the_title(); ?></h1>
+echo beans_open_markup( 'woo_single_title', 'h1', array(
+	'itemprop' => 'name',
+	'class' => 'product_title entry-title'
+) );
+
+	the_title();
+
+echo beans_close_markup( 'woo_single_title', 'h1' );
