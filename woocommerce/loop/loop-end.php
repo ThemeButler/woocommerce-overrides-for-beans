abcbd1d134be20ce72c750b034ebf109181c9135
Chris Rault <chris@devignerforhire.com>
2015-11-30 11:23:26 +0200

    Rewrote loop-end.php with Beans markup

--- woocommerce/loop/loop-end.php
+++ woocommerce/loop/loop-end.php
@@ -6,5 +6,9 @@
  * @package 	WooCommerce/Templates
  * @version     2.0.0
  */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
 ?>
-</ul>
+
+<?php echo beans_close_markup( 'woo_products_wrap', 'ul' ); ?>
