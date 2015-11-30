725a6822a1b072fd27a59864b19eaf46e6dbb195
Chris Rault <chris@devignerforhire.com>
2015-11-30 11:32:19 +0200

    Rewrote orderby.php with Beans markup

--- woocommerce/loop/orderby.php
+++ woocommerce/loop/orderby.php
@@ -7,30 +7,65 @@
  * @version     2.2.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-?>
-<form class="woocommerce-ordering" method="get">
-	<select name="orderby" class="orderby">
-		<?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
-			<option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
-		<?php endforeach; ?>
-	</select>
-	<?php
-		// Keep query string vars intact
-		foreach ( $_GET as $key => $val ) {
-			if ( 'orderby' === $key || 'submit' === $key ) {
+echo beans_open_markup( 'woo_orderby_form', 'form', array(
+	'class' => 'woocommerce-ordering',
+	'method' => 'get'
+) );
+
+	echo beans_open_markup( 'woo_orderby_select', 'select', array(
+		'name' => 'orderby',
+		'class' => 'orderby'
+	) );
+
+        foreach ( $catalog_orderby_options as $id => $name ) :
+
+            echo beans_open_markup( 'woo_orderby_option', 'option', array(
+				'value' => esc_attr( $id ),
+				'selected' => selected( $orderby, $id ) ? 'selected' : null
+				#TODO Double check
+			 ) );
+
+                echo esc_html( $name );
+
+            echo beans_open_markup( 'woo_orderby_option', 'option' );
+
+        endforeach;
+
+    echo beans_open_markup( 'woo_orderby_select', 'select' );
+
+    	// Keep query string vars intact
+		foreach ( $_GET as $key => $val ) :
+
+			if ( 'orderby' === $key || 'submit' === $key ) :
+
 				continue;
-			}
-			if ( is_array( $val ) ) {
-				foreach( $val as $innerVal ) {
-					echo '<input type="hidden" name="' . esc_attr( $key ) . '[]" value="' . esc_attr( $innerVal ) . '" />';
-				}
-			} else {
-				echo '<input type="hidden" name="' . esc_attr( $key ) . '" value="' . esc_attr( $val ) . '" />';
-			}
-		}
-	?>
-</form>
+
+			endif;
+
+			if ( is_array( $val ) ) :
+
+				foreach( $val as $innerVal ) :
+
+					echo beans_selfclose_markup( 'woo_orderby_option_hidden', 'input', array(
+						'type' => 'hidden',
+						'name' => esc_attr( $key ) . '[]',
+						'value' => esc_attr( $innerVal )
+					 ) );
+
+				endforeach;
+
+			else :
+
+				echo beans_selfclose_markup( 'woo_orderby_option_hidden', 'input', array(
+					'type' => 'hidden',
+					'name' => esc_attr( $key ),
+					'value' => esc_attr( $val )
+				 ) );
+
+			endif;
+
+		endforeach;
+
+echo beans_close_markup( 'woo_orderby_form', 'form' );
