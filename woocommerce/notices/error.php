9891bccb1faf1f7b7d8208a115cf7615ecf1fc9e
Chris Rault <chris@devignerforhire.com>
2015-11-30 12:51:55 +0200

    Rewrote error.php with Beans markup

--- woocommerce/notices/error.php
+++ woocommerce/notices/error.php
@@ -7,17 +7,24 @@
  * @version     1.6.4
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-if ( ! $messages ){
+if ( ! $messages ) :
+
 	return;
-}
 
-?>
-<ul class="woocommerce-error">
-	<?php foreach ( $messages as $message ) : ?>
-		<li><?php echo wp_kses_post( $message ); ?></li>
-	<?php endforeach; ?>
-</ul>
+endif;
+
+echo beans_open_markup( 'woo_error_list', 'ul', array( 'class' => 'woocommerce-error' ) );
+
+	foreach ( $messages as $message ) :
+
+		echo beans_open_markup( 'woo_error_list_item', 'li' );
+
+			echo wp_kses_post( $message );
+
+		echo beans_close_markup( 'woo_error_list_item', 'li' );
+
+	endforeach;
+
+echo beans_open_markup( 'woo_error_list', 'ul', array( 'class' => 'woocommerce-error' ) );
