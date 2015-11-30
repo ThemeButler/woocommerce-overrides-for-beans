4df03932d68caeeb12ae925420207e103d77f1e2
Chris Rault <chris@devignerforhire.com>
2015-11-30 13:13:52 +0200

    Updated the php block formatting

--- woocommerce/content-widget-product.php
+++ woocommerce/content-widget-product.php
@@ -11,29 +11,27 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-?>
+global $product;
 
-<?php global $product; ?>
+echo beans_open_markup( 'woo_product_widget_list_item', 'li' );
 
-<?php echo beans_open_markup( 'woo_product_widget_list_item', 'li' ); ?>
-
-	<?php echo beans_open_markup( 'woo_product_widget_list_item_link', 'a', array(
+	echo beans_open_markup( 'woo_product_widget_list_item_link', 'a', array(
 		'href' => esc_url( get_permalink( $product->id ) ),
 		'title' => esc_attr( $product->get_title() )
-	) ); ?>
+	) );
 
-		<?php echo $product->get_image(); ?>
+		echo $product->get_image();
 
-		<?php echo beans_open_markup( 'woo_product_widget_list_item_title', 'span', array( 'class' => 'product-title' ) ); ?>
+		echo beans_open_markup( 'woo_product_widget_list_item_title', 'span', array( 'class' => 'product-title' ) );
 
-			<?php echo $product->get_title(); ?>
+			echo $product->get_title();
 
-		<?php echo beans_close_markup( 'woo_product_widget_list_item_title', 'span' ); ?>
+		echo beans_close_markup( 'woo_product_widget_list_item_title', 'span' );
 
-	<?php echo beans_close_markup( 'woo_product_widget_list_item_link', 'a' ); ?>
+	echo beans_close_markup( 'woo_product_widget_list_item_link', 'a' );
 
-	<?php if ( ! empty( $show_rating ) ) echo $product->get_rating_html(); ?>
+	if ( ! empty( $show_rating ) ) echo $product->get_rating_html();
 
-	<?php echo $product->get_price_html(); ?>
+	echo $product->get_price_html();
 
-<?php echo beans_close_markup( 'woo_product_widget_list_item', 'li' ); ?>
+echo beans_close_markup( 'woo_product_widget_list_item', 'li' );
