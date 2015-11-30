8583ba5d167e3b7bd4c87e39c8b421d471f4d47b
Chris Rault <chris@devignerforhire.com>
2015-11-30 14:08:45 +0200

    Rewrote product-image.php with Beans markup

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -7,43 +7,47 @@
  * @version     2.0.14
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $post, $woocommerce, $product;
 
-?>
-<div class="images">
+echo beans_open_markup( 'woo_single_images_wrap', 'div', array( 'class' => 'images' ) );
 
-	<?php
-		if ( has_post_thumbnail() ) {
+	if ( has_post_thumbnail() ) :
 
-			$image_title 	= esc_attr( get_the_title( get_post_thumbnail_id() ) );
-			$image_caption 	= get_post( get_post_thumbnail_id() )->post_excerpt;
-			$image_link  	= wp_get_attachment_url( get_post_thumbnail_id() );
-			$image       	= get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ), array(
+			$image_title = esc_attr( get_the_title( get_post_thumbnail_id() ) );
+			$image_caption = get_post( get_post_thumbnail_id() )->post_excerpt;
+			$image_link = wp_get_attachment_url( get_post_thumbnail_id() );
+			$image = get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ), array(
 				'title'	=> $image_title,
-				'alt'	=> $image_title
-				) );
-
+				'alt' => $image_title
+			) );
 			$attachment_count = count( $product->get_gallery_attachment_ids() );
 
-			if ( $attachment_count > 0 ) {
+			if ( $attachment_count > 0 ) :
+
 				$gallery = '[product-gallery]';
-			} else {
+
+			else :
+
 				$gallery = '';
-			}
 
-			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<a href="%s" itemprop="image" class="woocommerce-main-image zoom" title="%s" data-rel="prettyPhoto' . $gallery . '">%s</a>', $image_link, $image_caption, $image ), $post->ID );
+			endif;
 
-		} else {
+			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( beans_open_markup( 'woo_single_image_link', 'a', array(
+				'href' => '%s',
+				'itemprop' => 'image',
+				'class' => 'woocommerce-main-image zoom',
+				'title' => '%s',
+				'data-rel' => 'prettyPhoto' . $gallery ) ) . '%s' . beans_close_markup( 'woo_single_image_link', 'a' ), $image_link, $image_caption, $image ), $post->ID );
 
-			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="%s" />', wc_placeholder_img_src(), __( 'Placeholder', 'woocommerce' ) ), $post->ID );
+		else :
 
-		}
-	?>
+			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( beans_selfclose_markup( 'woo_single_image_product_image', 'img', array(
+				'src' => '%s','alt' => '%s' ) ), wc_placeholder_img_src(), __( 'Placeholder', 'woocommerce' ) ), $post->ID );
 
-	<?php do_action( 'woocommerce_product_thumbnails' ); ?>
+		endif;
 
-</div>
+	do_action( 'woocommerce_product_thumbnails' );
+
+echo beans_close_markup( 'woo_single_images_wrap', 'div' );
