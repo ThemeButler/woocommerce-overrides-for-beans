9dcc38180c095e8482cd223895d2109e447262ae
Chris Rault <chris@devignerforhire.com>
2015-11-30 14:31:50 +0200

    Rewrote share.php with Beans markup

--- woocommerce/single-product/share.php
+++ woocommerce/single-product/share.php
@@ -9,10 +9,6 @@
  * @version     1.6.4
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-?>
-
-<?php do_action( 'woocommerce_share' ); // Sharing plugins can hook into here ?>
+do_action( 'woocommerce_share' ); // Sharing plugins can hook into here
