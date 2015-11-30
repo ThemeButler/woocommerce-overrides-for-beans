09452bbcb307861954aeb573d815382efb5ec0fa
Chris Rault <chris@devignerforhire.com>
2015-11-30 12:55:58 +0200

    Rewrote order-again.php with Beans markup

--- woocommerce/order/order-again.php
+++ woocommerce/order/order-again.php
@@ -7,11 +7,17 @@
  * @version 2.3.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
-?>
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-<p class="order-again">
-	<a href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'order_again', $order->id ) , 'woocommerce-order_again' ) ); ?>" class="button"><?php _e( 'Order Again', 'woocommerce' ); ?></a>
-</p>
+echo beans_open_markup( 'woo_order_again_wrap', 'p', array( 'class' = 'order-again' ) );
+
+	echo beans_open_markup( 'woo_order_again_link', 'a', array(
+		'href' = esc_url( wp_nonce_url( add_query_arg( 'order_again', $order->id ) , 'woocommerce-order_again' ) ),
+		'class' => 'button'
+	) );
+
+		_e( 'Order Again', 'woocommerce' );
+
+	echo beans_close_markup( 'woo_order_again_link', 'a' );
+
+echo beans_close_markup( 'woo_order_again_wrap', 'p' );
