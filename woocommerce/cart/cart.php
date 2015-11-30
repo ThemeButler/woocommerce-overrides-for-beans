a31e86918332196c7b28ebd7566a9243cd8a4872
Chris Rault <chris@devignerforhire.com>
2015-11-30 13:33:36 +0200

    Updated the php block formatting

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -11,69 +11,67 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 wc_print_notices();
 
-do_action( 'woocommerce_before_cart' ); ?>
+do_action( 'woocommerce_before_cart' );
 
-<?php echo beans_open_markup( 'woo_cart_form', 'form', array(
+echo beans_open_markup( 'woo_cart_form', 'form', array(
 	'action' => esc_url( WC()->cart->get_cart_url() ),
 	'method' => 'post'
-) ); ?>
+) );
 
-<?php do_action( 'woocommerce_before_cart_table' ); ?>
+do_action( 'woocommerce_before_cart_table' );
 
-<?php echo beans_open_markup( 'woo_cart_table', 'table', array(
+echo beans_open_markup( 'woo_cart_table', 'table', array(
 	'class' => 'shop_table cart',
 	'cellspacing' => 0
-) ); ?>
+) );
 
-	<?php echo beans_open_markup( 'woo_cart_table_heading', 'thead' ); ?>
+	echo beans_open_markup( 'woo_cart_table_heading', 'thead' );
 
-		<?php echo beans_open_markup( 'woo_cart_table_heading_row', 'tr' ); ?>
+		echo beans_open_markup( 'woo_cart_table_heading_row', 'tr' );
 
-			<?php echo beans_open_markup( 'woo_cart_table_heading_remove', 'th', array( 'class' => 'product-remove' ) ); ?>
-
-				&nbsp;
-
-			<?php echo beans_close_markup( 'woo_cart_table_heading_remove', 'th' ); ?>
-
-			<?php echo beans_open_markup( 'woo_cart_table_heading_thumbnail', 'th', array( 'class' => 'product-thumbnail' ) ); ?>
+			echo beans_open_markup( 'woo_cart_table_heading_remove', 'th', array( 'class' => 'product-remove' ) );
 
 				&nbsp;
 
-			<?php echo beans_close_markup( 'woo_cart_table_heading_thumbnail', 'th' ); ?>
+			echo beans_close_markup( 'woo_cart_table_heading_remove', 'th' );
 
-			<?php echo beans_open_markup( 'woo_cart_table_heading_name', 'th', array( 'class' => 'product-name' ) ); ?>
+			echo beans_open_markup( 'woo_cart_table_heading_thumbnail', 'th', array( 'class' => 'product-thumbnail' ) );
 
-				<?php _e( 'Product', 'woocommerce' ); ?>
+				&nbsp;
 
-			<?php echo beans_close_markup( 'woo_cart_table_heading_name', 'th' ); ?>
+			echo beans_close_markup( 'woo_cart_table_heading_thumbnail', 'th' );
 
-			<?php echo beans_open_markup( 'woo_cart_table_heading_price', 'th', array( 'class' => 'product-price' ) ); ?>
+			echo beans_open_markup( 'woo_cart_table_heading_name', 'th', array( 'class' => 'product-name' ) );
 
-				<?php _e( 'Price', 'woocommerce' ); ?>
+				_e( 'Product', 'woocommerce' );
 
-			<?php echo beans_close_markup( 'woo_cart_table_heading_price', 'th' ); ?>
+			echo beans_close_markup( 'woo_cart_table_heading_name', 'th' );
 
-			<?php echo beans_open_markup( 'woo_cart_table_heading_quantity', 'th', array( 'class' => 'product-quantity' ) ); ?>
+			echo beans_open_markup( 'woo_cart_table_heading_price', 'th', array( 'class' => 'product-price' ) );
 
-				<?php _e( 'Quantity', 'woocommerce' ); ?>
+				_e( 'Price', 'woocommerce' );
 
-			<?php echo beans_close_markup( 'woo_cart_table_heading_quantity', 'th' ); ?>
+			echo beans_close_markup( 'woo_cart_table_heading_price', 'th' );
 
-			<?php echo beans_open_markup( 'woo_cart_table_heading_subtotal', 'th', array( 'class' => 'product-subtotal' ) ); ?>
+			echo beans_open_markup( 'woo_cart_table_heading_quantity', 'th', array( 'class' => 'product-quantity' ) );
 
-				<?php _e( 'Total', 'woocommerce' ); ?>
+				_e( 'Quantity', 'woocommerce' );
 
-			<?php echo beans_close_markup( 'woo_cart_table_heading_subtotal', 'th' ); ?>
+			echo beans_close_markup( 'woo_cart_table_heading_quantity', 'th' );
 
-		<?php echo beans_close_markup( 'woo_cart_table_heading_row', 'tr' ); ?>
+			echo beans_open_markup( 'woo_cart_table_heading_subtotal', 'th', array( 'class' => 'product-subtotal' ) );
 
-	<?php echo beans_close_markup( 'woo_cart_form_thead', 'thead' ); ?>
+				_e( 'Total', 'woocommerce' );
 
-	<?php echo beans_open_markup( 'woo_cart_table_body', 'tbody' ); ?>
+			echo beans_close_markup( 'woo_cart_table_heading_subtotal', 'th' );
 
-		<?php do_action( 'woocommerce_before_cart_contents' ); ?>
+		echo beans_close_markup( 'woo_cart_table_heading_row', 'tr' );
 
-		<?php
+	echo beans_close_markup( 'woo_cart_form_thead', 'thead' );
+
+	echo beans_open_markup( 'woo_cart_table_body', 'tbody' );
+
+		do_action( 'woocommerce_before_cart_contents' );
 
 		foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) :
 
@@ -81,15 +79,13 @@ do_action( 'woocommerce_before_cart' ); ?>
 
 			$product_id   = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
 
-			if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) : ?>
+			if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) :
 
-				<?php echo beans_open_markup( 'woo_cart_table_filters_row', 'tr', array(
+				echo beans_open_markup( 'woo_cart_table_filters_row', 'tr', array(
 					'class' => esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) )
-					) ); ?>
+					) );
 
-					<?php echo beans_open_markup( 'woo_cart_table_cell_remove', 'td', array( 'class' => 'product-remove' ) ); ?>
-
-						<?php
+					echo beans_open_markup( 'woo_cart_table_cell_remove', 'td', array( 'class' => 'product-remove' ) );
 
 							echo apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
 								beans_open_markup( 'woo_cart_item_remove_link', 'a', array(
@@ -105,67 +101,55 @@ do_action( 'woocommerce_before_cart' ); ?>
 								esc_attr( $_product->get_sku() )
 							), $cart_item_key );
 
-						?>
+					echo beans_close_markup( 'woo_cart_table_cell_remove', 'td' );
 
-					<?php echo beans_close_markup( 'woo_cart_table_cell_remove', 'td' ); ?>
+					echo beans_open_markup( 'woo_cart_table_cell_thumbnail', 'td', array( 'class' => 'product-thumbnail' ) );
 
-					<?php echo beans_open_markup( 'woo_cart_table_cell_thumbnail', 'td', array( 'class' => 'product-thumbnail' ) ); ?>
+						$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
 
-						<?php
+						if ( ! $_product->is_visible() ) :
 
-							$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+							echo $thumbnail;
 
-							if ( ! $_product->is_visible() ) :
+						else :
 
-								echo $thumbnail;
+							printf( '<a href="%s">%s</a>', esc_url( $_product->get_permalink( $cart_item ) ), $thumbnail );
 
-							else :
+						endif;
 
-								printf( '<a href="%s">%s</a>', esc_url( $_product->get_permalink( $cart_item ) ), $thumbnail );
+					echo beans_close_markup( 'woo_cart_table_cell_thumbnail', 'td' );
 
-							endif;
+					echo beans_open_markup( 'woo_cart_table_cell_name', 'td', array( 'class' => 'product-name' ) );
 
-						?>
+						if ( ! $_product->is_visible() ) :
 
-					<?php echo beans_close_markup( 'woo_cart_table_cell_thumbnail', 'td' ); ?>
+							echo apply_filters( 'woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key ) . '&nbsp;';
 
-					<?php echo beans_open_markup( 'woo_cart_table_cell_name', 'td', array( 'class' => 'product-name' ) ); ?>
+						else :
 
-						<?php
+							echo apply_filters( 'woocommerce_cart_item_name', sprintf( beans_open_markup( 'woo_cart_item_name_link', 'a', array( 'href' => '%s' ) )'%s ' . beans_close_markup( 'woo_cart_item_name_link', 'a' ), esc_url( $_product->get_permalink( $cart_item ) ), $_product->get_title() ), $cart_item, $cart_item_key );
 
-							if ( ! $_product->is_visible() ) :
+						endif;
 
-								echo apply_filters( 'woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key ) . '&nbsp;';
+						// Meta data
+						echo WC()->cart->get_item_data( $cart_item );
 
-							else :
+						// Backorder notification
+						if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) :
 
-								echo apply_filters( 'woocommerce_cart_item_name', sprintf( beans_open_markup( 'woo_cart_item_name_link', 'a', array( 'href' => '%s' ) )'%s ' . beans_close_markup( 'woo_cart_item_name_link', 'a' ), esc_url( $_product->get_permalink( $cart_item ) ), $_product->get_title() ), $cart_item, $cart_item_key );
+							echo beans_open_markup( 'woo_cart_backorder_notification', 'p', array( 'class' => 'backorder_notification' ) ) . esc_html__( 'Available on backorder', 'woocommerce' ) . beans_close_markup( 'woo_cart_backorder_notification', 'p' );
 
-							endif;
+						endif;
 
-							// Meta data
-							echo WC()->cart->get_item_data( $cart_item );
+					echo beans_close_markup( 'woo_cart_table_cell_name', 'td' );
 
-							// Backorder notification
-							if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) :
+					echo beans_open_markup( 'woo_cart_table_cell_price', 'td', array( 'class' => 'product-price' ) );
 
-								echo beans_open_markup( 'woo_cart_backorder_notification', 'p', array( 'class' => 'backorder_notification' ) ) . esc_html__( 'Available on backorder', 'woocommerce' ) . beans_close_markup( 'woo_cart_backorder_notification', 'p' );
+						echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key );
 
-							endif;
+					echo beans_close_markup( 'woo_cart_table_cell_price', 'td' );
 
-						?>
-
-					<?php echo beans_close_markup( 'woo_cart_table_cell_name', 'td' ); ?>
-
-					<?php echo beans_open_markup( 'woo_cart_table_cell_price', 'td', array( 'class' => 'product-price' ) ); ?>
-
-						<?php echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); ?>
-
-					<?php echo beans_close_markup( 'woo_cart_table_cell_price', 'td' ); ?>
-
-					<?php echo beans_open_markup( 'woo_cart_table_cell_quantity', 'td', array( 'class' => 'product-quantity' ) ); ?>
-
-						<?php
+					echo beans_open_markup( 'woo_cart_table_cell_quantity', 'td', array( 'class' => 'product-quantity' ) );
 
 							if ( $_product->is_sold_individually() ) :
 
@@ -185,94 +169,90 @@ do_action( 'woocommerce_before_cart' ); ?>
 
 							echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item );
 
-						?>
+					echo beans_close_markup( 'woo_cart_table_cell_quantity', 'td' );
 
-					<?php echo beans_close_markup( 'woo_cart_table_cell_quantity', 'td' ); ?>
+					echo beans_open_markup( 'woo_cart_table_cell_subtotals', 'td', array( 'class' => 'product-subtotals' ) );
 
-					<?php echo beans_open_markup( 'woo_cart_table_cell_subtotals', 'td', array( 'class' => 'product-subtotals' ) ); ?>
+						echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key );
 
-						<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); ?>
+					echo beans_close_markup( 'woo_cart_table_cell_subtotals', 'td' );
 
-					<?php echo beans_close_markup( 'woo_cart_table_cell_subtotals', 'td' ); ?>
-
-				<?php echo beans_open_markup( 'woo_cart_table_filters_row', 'tr' ); ?>
-
-				<?php
+				echo beans_open_markup( 'woo_cart_table_filters_row', 'tr' );
 
 			endif;
 
 		endforeach;
 
-		do_action( 'woocommerce_cart_contents' ); ?>
+		do_action( 'woocommerce_cart_contents' );
 
-		<?php echo beans_open_markup( 'woo_cart_contents_row', 'tr' ); ?>
+		echo beans_open_markup( 'woo_cart_contents_row', 'tr' );
 
-		<?php echo beans_open_markup( 'woo_cart_contents_actions', 'td', array(
+		echo beans_open_markup( 'woo_cart_contents_actions', 'td', array(
 			'colspan' => 6,
 			'class' => 'actions'
-		) ); ?>
+		) );
 
-				<?php if ( WC()->cart->coupons_enabled() ) : ?>
+				if ( WC()->cart->coupons_enabled() ) :
 
-					<?php echo beans_open_markup( 'woo_cart_contents_coupon', 'div', array( 'class' => 'coupon' ) ); ?>
+					echo beans_open_markup( 'woo_cart_contents_coupon', 'div', array( 'class' => 'coupon' ) );
 
-						<?php echo beans_open_markup( 'woo_cart_contents_coupon_label', 'label', array( 'for' => 'coupon_code' ) ); ?>
+						echo beans_open_markup( 'woo_cart_contents_coupon_label', 'label', array( 'for' => 'coupon_code' ) );
 
-                            <?php _e( 'Coupon', 'woocommerce' ); ?>:
+                            _e( 'Coupon', 'woocommerce' );:
 
-						<?php echo beans_close_markup( 'woo_cart_contents_coupon_label', 'label' ); ?>
+						echo beans_close_markup( 'woo_cart_contents_coupon_label', 'label' );
 
-						<?php echo beans_selfclose_markup( 'woo_cart_contents_coupon_input', 'input', array(
+						echo beans_selfclose_markup( 'woo_cart_contents_coupon_input', 'input', array(
 							'type' => 'text',
 							'name' => 'coupon_code',
 							'class' => 'input-text',
 							'id' => 'coupon_code',
 							'value' => '',
 							'placeholder' => esc_attr_e( 'Coupon code', 'woocommerce' )
-						) ); ?>
+						) );
 
-						<?php echo beans_selfclose_markup( 'woo_cart_contents_coupon_button', 'input', array(
+						echo beans_selfclose_markup( 'woo_cart_contents_coupon_button', 'input', array(
 							'type' => 'submit',
 							'class' => 'button',
 							'name' => 'apply_coupon',
 							'value' => esc_attr_e( 'Apply Coupon', 'woocommerce' )
-						) ); ?>
+						) );
 
-						<?php do_action( 'woocommerce_cart_coupon' ); ?>
+						do_action( 'woocommerce_cart_coupon' );
 
-					<?php echo beans_close_markup( 'woo_cart_contents_coupon', 'div' ); ?>
+					echo beans_close_markup( 'woo_cart_contents_coupon', 'div' );
 
-				<?php endif; ?>
+				endif;
 
-				<?php echo beans_selfclose_markup( 'woo_cart_contents_button', 'td', array(
+				echo beans_selfclose_markup( 'woo_cart_contents_button', 'td', array(
 					'type' => 'submit',
 					'class' => 'button',
 					'name' => 'update_cart',
 					'value' => esc_attr_e( 'Update Cart', 'woocommerce' )
-				) ); ?>
+				) );
 
-				<?php do_action( 'woocommerce_cart_actions' ); ?>
+				do_action( 'woocommerce_cart_actions' );
 
-				<?php wp_nonce_field( 'woocommerce-cart' ); ?>
+				wp_nonce_field( 'woocommerce-cart' );
 
-			<?php echo beans_close_markup( 'woo_cart_contents_actions', 'td' ); ?>
+			echo beans_close_markup( 'woo_cart_contents_actions', 'td' );
 
-		<?php echo beans_close_markup( 'woo_cart_contents_row', 'tr' ); ?>
+		echo beans_close_markup( 'woo_cart_contents_row', 'tr' );
 
-		<?php do_action( 'woocommerce_after_cart_contents' ); ?>
+		do_action( 'woocommerce_after_cart_contents' );
 
-	<?php echo beans_open_markup( 'woo_cart_table_body', 'tbody' ); ?>
+	echo beans_open_markup( 'woo_cart_table_body', 'tbody' );
 
-<?php echo beans_close_markup( 'woo_cart_table', 'table' ); ?>
+echo beans_close_markup( 'woo_cart_table', 'table' );
 
-<?php do_action( 'woocommerce_after_cart_table' ); ?>
+do_action( 'woocommerce_after_cart_table' );
 
-<?php echo beans_close_markup( 'woo_cart_form', 'form' ); ?>
+echo beans_close_markup( 'woo_cart_form', 'form' );
 
-<?php echo beans_open_markup( 'woo_cart_contents_collaterals', 'div', array( 'class' => 'cart-collaterals' ) ); ?>
+echo beans_open_markup( 'woo_cart_contents_collaterals', 'div', array( 'class' => 'cart-collaterals' ) );
 
-	<?php do_action( 'woocommerce_cart_collaterals' ); ?>
+	do_action( 'woocommerce_cart_collaterals' );
 
-<?php echo beans_close_markup( 'woo_cart_contents_collaterals', 'div' ); ?>
+echo beans_close_markup( 'woo_cart_contents_collaterals', 'div' );
 
-<?php do_action( 'woocommerce_after_cart' ); ?>
+do_action( 'woocommerce_after_cart' );
