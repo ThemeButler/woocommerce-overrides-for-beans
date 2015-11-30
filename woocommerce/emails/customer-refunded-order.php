7ce7be818990c2e4e12006dc43b63a6878755f42
Chris Rault <chris@devignerforhire.com>
2015-11-30 10:50:31 +0200

    Rewrote customer-refunded-order.php with Beans markup

--- woocommerce/emails/customer-refunded-order.php
+++ woocommerce/emails/customer-refunded-order.php
@@ -13,55 +13,160 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 <?php do_action( 'woocommerce_email_header', $email_heading ); ?>
 
-<p><?php
-	if ( $partial_refund ) {
+<?php echo beans_open_markup( 'woo_emails_customer_refund_notice', 'p' ); ?>
+
+	<?php
+
+	if ( $partial_refund ) :
+
 		printf( __( "Hi there. Your order on %s has been partially refunded.", 'woocommerce' ), get_option( 'blogname' ) );
-	}
-	else {
+
+	else :
+
 		printf( __( "Hi there. Your order on %s has been refunded.", 'woocommerce' ), get_option( 'blogname' ) );
-	}
-?></p>
+
+	endif;
+
+	?>
+
+<?php echo beans_close_markup( 'woo_emails_customer_refund_notice', 'p' ); ?>
+
 
 <?php do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plain_text ); ?>
 
-<h2><?php printf( __( 'Order #%s', 'woocommerce' ), $order->get_order_number() ); ?></h2>
+<?php echo beans_open_markup( 'woo_emails_customer_refund_title', 'h2' ); ?>
 
-<table class="td" cellspacing="0" cellpadding="6" style="width: 100%; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" border="1">
-	<thead>
-		<tr>
-			<th class="td" scope="col" style="text-align:left;"><?php _e( 'Product', 'woocommerce' ); ?></th>
-			<th class="td" scope="col" style="text-align:left;"><?php _e( 'Quantity', 'woocommerce' ); ?></th>
-			<th class="td" scope="col" style="text-align:left;"><?php _e( 'Price', 'woocommerce' ); ?></th>
-		</tr>
-	</thead>
-	<tbody>
+	<?php printf( __( 'Order #%s', 'woocommerce' ), $order->get_order_number() ); ?>
+
+<?php echo beans_close_markup( 'woo_emails_customer_refund_title', 'h2' ); ?>
+
+<?php echo beans_open_markup( 'woo_emails_customer_refund_table', 'table', array(
+	'class' => 'td',
+	'cellspacing' => 0,
+	'cellpadding' => 6,
+	'style' => 'width: 100%; font-family: \'Helvetica Neue\', Helvetica, Roboto, Arial, sans-serif;',
+	'border' => 0
+) );
+?>
+
+	<?php echo beans_open_markup( 'woo_emails_customer_refund_table_head', 'thead' ); ?>
+
+		<?php echo beans_open_markup( 'woo_emails_customer_refund_table_heading_row', 'tr' ); ?>
+
+			<?php echo beans_open_markup( 'woo_emails_customer_refund_table_heading_product', 'th', array(
+				'class' => 'td',
+				'scope' => 'col',
+				'style' => 'text-align:left;'
+			) ); ?>
+
+				<?php _e( 'Product', 'woocommerce' ); ?>
+
+			<?php echo beans_close_markup( 'woo_emails_customer_refund_table_heading_product', 'th' ); ?>
+
+			<?php echo beans_open_markup( 'woo_emails_customer_refund_table_heading_quantity', 'th', array(
+				'class' => 'td',
+				'scope' => 'col',
+				'style' => 'text-align:left;'
+			) ); ?>
+
+				<?php _e( 'Quantity', 'woocommerce' ); ?>
+
+			<?php echo beans_close_markup( 'woo_emails_customer_refund_table_heading_quantity', 'th' ); ?>
+
+			<?php echo beans_open_markup( 'woo_emails_customer_refund_table_heading_price', 'th', array(
+				'class' => 'td',
+				'scope' => 'col',
+				'style' => 'text-align:left;'
+			) ); ?>
+
+				<?php _e( 'Price', 'woocommerce' ); ?>
+
+			<?php echo beans_close_markup( 'woo_emails_customer_refund_table_heading_price', 'th' ); ?>
+
+		<?php echo beans_close_markup( 'woo_emails_customer_refund_table_heading_row', 'tr' ); ?>
+
+	<?php echo beans_close_markup( 'woo_emails_customer_refund_table_head', 'thead' ); ?>
+
+	<?php echo beans_open_markup( 'woo_emails_customer_refund_table_body', 'tbody' ); ?>
+
 		<?php echo $order->email_order_items_table( true, false, true ); ?>
-	</tbody>
-	<tfoot>
-		<?php
-			if ( $totals = $order->get_order_item_totals() ) {
+
+	<?php echo beans_close_markup( 'woo_emails_customer_refund_table_body', 'tbody' ); ?>
+
+	<?php echo beans_open_markup( 'woo_emails_customer_refund_table_foot', 'tfoot' ); ?>
+
+		<?php if ( $totals = $order->get_order_item_totals() ) :
 
 				$i = 0;
 
-				if ( $refund && $refund->get_refund_amount() > 0 ) {
-					?><tr>
-						<th scope="row" colspan="2" style="text-align:left; border: 1px solid #eee;border-top-width: 4px;"><?php _e( 'Amount Refunded', 'woocommerce' ); ?>:</th>
-						<td style="text-align:left; border: 1px solid #eee;border-top-width: 4px;"><?php echo $refund->get_formatted_refund_amount(); ?></td>
-					</tr><?php
-					$i++;
-				}
+				if ( $refund && $refund->get_refund_amount() > 0 ) : ?>
 
-				foreach ( $totals as $total ) {
+					<?php echo beans_open_markup( 'woo_emails_customer_refund_table_foot_row', 'tr' ); ?>
+
+						<?php echo beans_open_markup( 'woo_emails_customer_refund_table_foot_label', 'th', array(
+							'scope' => 'row',
+							'colspan' => 2,
+							'style' => 'text-align:left; border: 1px solid #eee;border-top-width: 4px;'
+						) ); ?>
+
+							<?php _e( 'Amount Refunded', 'woocommerce' ); ?>:
+
+						<?php echo beans_close_markup( 'woo_emails_customer_refund_table_foot_label', 'th' ); ?>
+
+						<?php echo beans_open_markup( 'woo_emails_customer_refund_table_foot_value', 'td', array(
+							'style' => 'text-align:left; border: 1px solid #eee;border-top-width: 4px;'
+						) ); ?>
+
+							<?php echo $refund->get_formatted_refund_amount(); ?>
+
+						<?php echo beans_close_markup( 'woo_emails_customer_refund_table_foot_value', 'td' ); ?>
+
+					<?php echo beans_close_markup( 'woo_emails_customer_refund_table_foot_row', 'tr' ); ?>
+
+					<?php
 					$i++;
-					?><tr>
-						<th class="td" scope="row" colspan="2" style="text-align:left; <?php if ( $i == 1 ) echo 'border-top-width: 4px;'; ?>"><?php echo $total['label']; ?></th>
-						<td class="td" style="text-align:left; <?php if ( $i == 1 ) echo 'border-top-width: 4px;'; ?>"><?php echo $total['value']; ?></td>
-					</tr><?php
-				}
-			}
+
+				endif;
+
+				foreach ( $totals as $total ) :
+					$i++;
+					?>
+
+					<?php echo beans_open_markup( 'woo_emails_customer_refund_table_foot_totals_row', 'tr' ); ?>
+
+						<?php echo beans_open_markup( 'woo_emails_customer_refund_table_foot_totals_label', 'th', array(
+							'class' => 'td',
+							'scope' => 'row',
+							'colspan' => 2,
+							'style' => 'text-align: left;' . $i == 1 ? ' border-top-width: 4px;' : null
+						) ); ?>
+
+							<?php echo $total['label']; ?>
+
+						<?php echo beans_close_markup( 'woo_emails_customer_refund_table_foot_totals_label', 'th' ); ?>
+
+						<?php echo beans_open_markup( 'woo_emails_customer_refund_table_foot_totals_value', 'td', array(
+							'class' => 'td',
+							'style' => 'text-align: left;' . $i == 1 ? ' border-top-width: 4px;' : null
+						) ); ?>
+
+							<?php echo $total['value']; ?>
+
+						<?php echo beans_close_markup( 'woo_emails_customer_refund_table_foot_totals_value', 'td' ); ?>
+
+					<?php echo beans_close_markup( 'woo_emails_customer_refund_table_foot_totals_row', 'tr' ); ?>
+
+					<?php
+
+				endforeach;
+
+			endif;
+
 		?>
-	</tfoot>
-</table>
+
+	<?php echo beans_close_markup( 'woo_emails_customer_refund_table_foot', 'tfoot' ); ?>
+
+<?php echo beans_close_markup( 'woo_emails_customer_refund_table', 'table' ); ?>
 
 <?php do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text ); ?>
 
