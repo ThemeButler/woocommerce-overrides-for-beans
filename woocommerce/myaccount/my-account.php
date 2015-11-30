22d91db0aa52ba2168b3f541ba058ef1bea08de7
Chris Rault <chris@devignerforhire.com>
2015-11-30 12:25:46 +0200

    Rewrote my-account.php with Beans markup

--- woocommerce/myaccount/my-account.php
+++ woocommerce/myaccount/my-account.php
@@ -7,32 +7,30 @@
  * @version     2.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-wc_print_notices(); ?>
+wc_print_notices();
 
-<p class="myaccount_user">
-	<?php
+echo beans_open_markup( 'woo_my_account_user', 'p', array( 'class' => 'myaccount_user' ) );
+
 	printf(
-		__( 'Hello <strong>%1$s</strong> (not %1$s? <a href="%2$s">Sign out</a>).', 'woocommerce' ) . ' ',
+		__( 'Hello ' . beans_open_markup( 'woo_my_account_user_name', 'strong' ) . '%1$s' . beans_close_markup( 'woo_my_account_user_name', 'strong' ) . ' (not %1$s? ' . beans_open_markup( 'woo_my_account_user_logout_link', 'a', array( 'href' => '%2$s' ) ) . 'Sign out' . beans_close_markup( 'woo_my_account_user_logout_link', 'a' ), 'woocommerce' ) . ' ',
 		$current_user->display_name,
 		wc_get_endpoint_url( 'customer-logout', '', wc_get_page_permalink( 'myaccount' ) )
 	);
 
-	printf( __( 'From your account dashboard you can view your recent orders, manage your shipping and billing addresses and <a href="%s">edit your password and account details</a>.', 'woocommerce' ),
+	printf( __( 'From your account dashboard you can view your recent orders, manage your shipping and billing addresses and ' . beans_open_markup( 'woo_my_account_user_account_link', 'a', array( 'href' => '%s' ) ) . 'edit your password and account details' . beans_close_markup( 'woo_my_account_user_account_link', 'a' ), 'woocommerce' ),
 		wc_customer_edit_account_url()
 	);
-	?>
-</p>
 
-<?php do_action( 'woocommerce_before_my_account' ); ?>
+echo beans_close_markup( 'woo_my_account_user', 'p' );
 
-<?php wc_get_template( 'myaccount/my-downloads.php' ); ?>
+do_action( 'woocommerce_before_my_account' );
 
-<?php wc_get_template( 'myaccount/my-orders.php', array( 'order_count' => $order_count ) ); ?>
+wc_get_template( 'myaccount/my-downloads.php' );
 
-<?php wc_get_template( 'myaccount/my-address.php' ); ?>
+wc_get_template( 'myaccount/my-orders.php', array( 'order_count' => $order_count ) );
 
-<?php do_action( 'woocommerce_after_my_account' ); ?>
+wc_get_template( 'myaccount/my-address.php' );
+
+do_action( 'woocommerce_after_my_account' );
