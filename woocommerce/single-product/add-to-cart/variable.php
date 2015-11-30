fb4cad0e13dace895574c4e90d14c794121e31c6
Chris Rault <chris@devignerforhire.com>
2015-11-30 14:55:27 +0200

    Rewrote variable.php with Beans markup

--- woocommerce/single-product/add-to-cart/variable.php
+++ woocommerce/single-product/add-to-cart/variable.php
@@ -6,67 +6,105 @@
  * @package WooCommerce/Templates
  * @version 2.4.0
  */
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $product;
 
 $attribute_keys = array_keys( $attributes );
 
-do_action( 'woocommerce_before_add_to_cart_form' ); ?>
+do_action( 'woocommerce_before_add_to_cart_form' );
 
-<form class="variations_form cart" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->id ); ?>" data-product_variations="<?php echo esc_attr( json_encode( $available_variations ) ) ?>">
-	<?php do_action( 'woocommerce_before_variations_form' ); ?>
+echo beans_open_markup( 'woo_single_add_to_cart_variable_form', 'form', array(
+	'class' => 'variations_form cart',
+	'method' => 'post',
+	'enctype' => 'multipart/form-data',
+	'data-product_id' => absint( $product->id ),
+	'data-product_variations' => esc_attr( json_encode( $available_variations ) )
+) );
 
-	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
-		<p class="stock out-of-stock"><?php _e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
-	<?php else : ?>
-		<table class="variations" cellspacing="0">
-			<tbody>
-				<?php foreach ( $attributes as $attribute_name => $options ) : ?>
-					<tr>
-						<td class="label"><label for="<?php echo sanitize_title( $attribute_name ); ?>"><?php echo wc_attribute_label( $attribute_name ); ?></label></td>
-						<td class="value">
-							<?php
-								$selected = isset( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ? wc_clean( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) : $product->get_variation_default_attribute( $attribute_name );
-								wc_dropdown_variation_attribute_options( array( 'options' => $options, 'attribute' => $attribute_name, 'product' => $product, 'selected' => $selected ) );
-								echo end( $attribute_keys ) === $attribute_name ? '<a class="reset_variations" href="#">' . __( 'Clear selection', 'woocommerce' ) . '</a>' : '';
-							?>
-						</td>
-					</tr>
-		        <?php endforeach;?>
-			</tbody>
-		</table>
+	do_action( 'woocommerce_before_variations_form' );
 
-		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+	if ( empty( $available_variations ) && false !== $available_variations ) :
 
-		<div class="single_variation_wrap" style="display:none;">
-			<?php
-				/**
-				 * woocommerce_before_single_variation Hook
-				 */
-				do_action( 'woocommerce_before_single_variation' );
+		echo beans_open_markup( 'woo_single_add_to_cart_variable_out_of_stock_message', 'p', array( 'class' => 'stock out-of-stock' ) );
 
-				/**
-				 * woocommerce_single_variation hook. Used to output the cart button and placeholder for variation data.
-				 * @since 2.4.0
-				 * @hooked woocommerce_single_variation - 10 Empty div for variation data.
-				 * @hooked woocommerce_single_variation_add_to_cart_button - 20 Qty and cart button.
-				 */
-				do_action( 'woocommerce_single_variation' );
+			_e( 'This product is currently out of stock and unavailable.', 'woocommerce' );
 
-				/**
-				 * woocommerce_after_single_variation Hook
-				 */
-				do_action( 'woocommerce_after_single_variation' );
-			?>
-		</div>
+		echo beans_close_markup( 'woo_single_add_to_cart_variable_out_of_stock_message', 'p' );
 
-		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
-	<?php endif; ?>
+	else :
 
-	<?php do_action( 'woocommerce_after_variations_form' ); ?>
-</form>
+		echo beans_open_markup( 'woo_single_add_to_cart_variable_table', 'table', array(
+			'class' => 'variations',
+			'cellspacing' => 0
+		) );
 
-<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
+			echo beans_open_markup( 'woo_single_add_to_cart_variable_tbody', 'tbody' );
+
+				foreach ( $attributes as $attribute_name => $options ) :
+
+					echo beans_open_markup( 'woo_single_add_to_cart_variable_tr', 'tr' );
+
+						echo beans_open_markup( 'woo_single_add_to_cart_variable_label_cell', 'td', array( 'class' => 'label' ) );
+
+							echo beans_open_markup( 'woo_single_add_to_cart_variable_label', 'label', array( 'for' => sanitize_title( $attribute_name ) ) );
+
+								echo wc_attribute_label( $attribute_name );
+
+							echo beans_open_markup( 'woo_single_add_to_cart_variable_label', 'label' );
+
+						echo beans_close_markup( 'woo_single_add_to_cart_variable_label_td', 'td' );
+
+						echo beans_open_markup( 'woo_single_add_to_cart_variable_value_cell', 'td', array( 'class' => 'value' ) );
+
+							$selected = isset( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ? wc_clean( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) : $product->get_variation_default_attribute( $attribute_name );
+
+							wc_dropdown_variation_attribute_options( array( 'options' => $options, 'attribute' => $attribute_name, 'product' => $product, 'selected' => $selected ) );
+
+							echo end( $attribute_keys ) === $attribute_name ? beans_open_markup( 'woo_single_add_to_cart_remove_variation_link', 'a', array( 'class' => 'reset_variations', 'href' => '#' ) ) . __( 'Clear selection', 'woocommerce' ) . beans_close_markup( 'woo_single_add_to_cart_remove_variation_link', 'a' ) : '';
+
+
+						echo beans_close_markup( 'woo_single_add_to_cart_variable_value_cell', 'td' );
+
+					echo beans_close_markup( 'woo_single_add_to_cart_variable_tr', 'tr' );
+
+		        endforeach;
+
+			echo beans_close_markup( 'woo_single_add_to_cart_variable_tbody', 'tbody' );
+
+		echo beans_close_markup( 'woo_single_add_to_cart_variable_table', 'table' );
+
+		do_action( 'woocommerce_before_add_to_cart_button' );
+
+		echo beans_open_markup( 'woo_single_add_to_cart_single_variation_wrap', 'div', array( 'class' => 'single_variation_wrap', 'style' => 'display:none;' ) );
+
+			/**
+			 * woocommerce_before_single_variation Hook
+			 */
+			do_action( 'woocommerce_before_single_variation' );
+
+			/**
+			 * woocommerce_single_variation hook. Used to output the cart button and placeholder for variation data.
+			 * @since 2.4.0
+			 * @hooked woocommerce_single_variation - 10 Empty div for variation data.
+			 * @hooked woocommerce_single_variation_add_to_cart_button - 20 Qty and cart button.
+			 */
+			do_action( 'woocommerce_single_variation' );
+
+			/**
+			 * woocommerce_after_single_variation Hook
+			 */
+			do_action( 'woocommerce_after_single_variation' );
+
+		echo beans_close_markup( 'woo_single_add_to_cart_single_variation_wrap', 'div' );
+
+		do_action( 'woocommerce_after_add_to_cart_button' );
+
+	endif;
+
+	do_action( 'woocommerce_after_variations_form' );
+
+echo beans_open_markup( 'woo_single_add_to_cart_variable_form', 'form' );
+
+do_action( 'woocommerce_after_add_to_cart_form' );
