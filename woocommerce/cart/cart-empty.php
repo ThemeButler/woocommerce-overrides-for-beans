b51b29c1fc19ffa023bb8e2920888c1a3f050d16
Chris Rault <chris@devignerforhire.com>
2015-11-30 13:29:26 +0200

    Updated the php block formatting

--- woocommerce/cart/cart-empty.php
+++ woocommerce/cart/cart-empty.php
@@ -11,25 +11,23 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 wc_print_notices();
 
-?>
+echo beans_open_markup( 'woo_cart_empty_notice', 'p', array( 'class' => 'cart-empty' ) );
 
-<?php echo beans_open_markup( 'woo_cart_empty_notice', 'p', array( 'class' => 'cart-empty' ) ); ?>
+    _e( 'Your cart is currently empty.', 'woocommerce' )
 
-    <?php _e( 'Your cart is currently empty.', 'woocommerce' ) ?>
+echo beans_close_markup( 'woo_product_item_link', 'p' );
 
-<?php echo beans_close_markup( 'woo_product_item_link', 'p' ); ?>
+do_action( 'woocommerce_cart_is_empty' );
 
-<?php do_action( 'woocommerce_cart_is_empty' ); ?>
+echo beans_open_markup( 'woo_cart_back_to_shop', 'p', array( 'class' => 'return-to-shop' ) );
 
-<?php echo beans_open_markup( 'woo_cart_back_to_shop', 'p', array( 'class' => 'return-to-shop' ) ); ?>
-
-    <?php echo beans_open_markup( 'woo_cart_back_to_shop_link', 'a', array(
+    echo beans_open_markup( 'woo_cart_back_to_shop_link', 'a', array(
         'href' => esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ),
         'class' => 'button wc-backward'
-    ) ); ?>
+    ) );
 
-        <?php _e( 'Return To Shop', 'woocommerce' ) ?>
+        _e( 'Return To Shop', 'woocommerce' )
 
-    <?php echo beans_open_markup( 'woo_cart_back_to_shop_link', 'a' ); ?>
+    echo beans_open_markup( 'woo_cart_back_to_shop_link', 'a' );
 
-<?php echo beans_close_markup( 'woo_cart_back_to_shop', 'p' ); ?>
+echo beans_close_markup( 'woo_cart_back_to_shop', 'p' );
