9e51dd16aae275ad322ae41586b91f842cb25698
Chris Rault <chris@devignerforhire.com>
2015-11-30 13:14:26 +0200

    Updated the php block formatting

--- woocommerce/product-searchform.php
+++ woocommerce/product-searchform.php
@@ -11,40 +11,38 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-?>
-
-<?php echo beans_open_markup( 'woo_product_search_form', 'form', array(
+echo beans_open_markup( 'woo_product_search_form', 'form', array(
 	'role' => 'search',
 	'method' => 'get',
 	'class' => 'woocommerce-product-search',
 	'action' => esc_url( home_url( '/' ) )
-) ); ?>
+) );
 
-	<?php echo beans_open_markup( 'woo_product_search_label', 'label', array(
+	echo beans_open_markup( 'woo_product_search_label', 'label', array(
 		'class' => 'screen-reader-text',
 		'for' => 's'
-	) ); ?>
+	) );
 
-		<?php _e( 'Search for:', 'woocommerce' ); ?>
+		_e( 'Search for:', 'woocommerce' );
 
-	<?php echo beans_close_markup( 'woo_product_search_label', 'label' ); ?>
+	echo beans_close_markup( 'woo_product_search_label', 'label' );
 
-	<?php echo beans_selfclose_markup( 'woo_product_search_input', 'input', array(
+	echo beans_selfclose_markup( 'woo_product_search_input', 'input', array(
 		'class' => 'search-field',
 		'placeholder' => esc_attr_x( 'Search Products&hellip;', 'placeholder', 'woocommerce' ),
 		'value' => get_search_query(),
 		'name' => 's', 'title' => esc_attr_x( 'Search for:', 'label', 'woocommerce' )
-	) ); ?>
+	) );
 
-	 <?php echo beans_selfclose_markup( 'woo_product_search_button', 'input', array(
+	 echo beans_selfclose_markup( 'woo_product_search_button', 'input', array(
 		 'type' => 'submit',
 		 'value' => esc_attr_x( 'Search', 'submit button', 'woocommerce' )
-	) ); ?>
+	) );
 
-	 <?php echo beans_selfclose_markup( 'woo_product_search_hidden_input', 'input', array(
+	 echo beans_selfclose_markup( 'woo_product_search_hidden_input', 'input', array(
 		 'type' => 'hidden',
 		 'name' => 'post_type',
 		 'value' => 'product'
-	 ) ); ?>
+	 ) );
 
-<?php echo beans_close_markup( 'woo_product_search_form', 'form' ); ?>
+echo beans_close_markup( 'woo_product_search_form', 'form' );
