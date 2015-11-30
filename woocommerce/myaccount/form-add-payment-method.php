3acd38af71c32d2fb946f170f69b5e0a63d901eb
Chris Rault <chris@devignerforhire.com>
2015-11-30 11:53:21 +0200

    Rewrote form-add-payment-method.php with Beans markup

--- woocommerce/myaccount/form-add-payment-method.php
+++ woocommerce/myaccount/form-add-payment-method.php
@@ -7,49 +7,89 @@
  * @version     2.1
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-?>
-<form id="add_payment_method" method="post">
-	<div id="payment">
-		<ul class="payment_methods methods">
-			<?php
-				if ( $available_gateways = WC()->payment_gateways->get_available_payment_gateways() ) {
+echo beans_open_markup( 'woo_account_add_payment_method_form', 'form', array(
+	'id' => 'add_payment_method',
+	'method' => 'post'
+) );
+
+	echo beans_open_markup( 'woo_account_add_payment_wrap', 'div', array( 'id' => 'payment' ) );
+
+		echo beans_open_markup( 'woo_account_payment_methods_list', 'ul', array( 'class' => 'payment_methods methods' ) );
+
+			if ( $available_gateways = WC()->payment_gateways->get_available_payment_gateways() ) :
+
 					// Chosen Method
 					if ( sizeof( $available_gateways ) )
 						current( $available_gateways )->set_current();
 
-					foreach ( $available_gateways as $gateway ) {
-						?>
-						<li class="payment_method_<?php echo $gateway->id; ?>">
-							<input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> />
-							<label for="payment_method_<?php echo $gateway->id; ?>"><?php echo $gateway->get_title(); ?> <?php echo $gateway->get_icon(); ?></label>
-							<?php
-								if ( $gateway->has_fields() || $gateway->get_description() ) {
-									echo '<div class="payment_box payment_method_' . $gateway->id . '" style="display:none;">';
+					foreach ( $available_gateways as $gateway ) :
+
+						echo beans_open_markup( 'woo_account_payment_methods_list_item', 'li', array( 'class' => 'payment_method_' . $gateway->id ) );
+
+							echo beans_selfclose_markup( 'woo_account_payment_methods_list_item_input', 'input', array(
+								'id' => 'payment_method_' . $gateway->id,
+								'type' => 'radio',
+								'class' => 'input-radio',
+								'name' => 'payment_method',
+								'value' => esc_attr( $gateway->id,
+								'checked' => checked( $gateway->chosen, true ) ? 'checked' : null ) );
+
+							echo beans_open_markup( 'woo_account_payment_methods_list_item_label', 'label', array( 'for' => 'payment_method_' . $gateway->id ) );
+
+								echo $gateway->get_title() . ' ' . $gateway->get_icon();
+
+							echo v( 'woo_account_payment_methods_list_item_label', 'label' );
+
+							if ( $gateway->has_fields() || $gateway->get_description() ) :
+
+								echo beans_open_markup( 'woo_account_payment_methods_hidden', 'div', array(
+									'class' => 'payment_box payment_method_' . $gateway->id,
+									'style' => 'display:none;'
+							 	) );
+
 									$gateway->payment_fields();
-									echo '</div>';
-								}
-							?>
-						</li>
-						<?php
-					}
-				} else {
 
-					echo '<p>' . __( 'Sorry, it seems that there are no payment methods which support adding a new payment method. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) . '</p>';
+								echo beans_close_markup( 'woo_account_payment_methods_hidden', 'div' );
 
-				}
-			?>
-		</ul>
+							endif;
 
-		<div class="form-row">
-			<?php wp_nonce_field( 'woocommerce-add-payment-method' ); ?>
-			<input type="submit" class="button alt" id="place_order" value="<?php esc_attr_e( 'Add Payment Method', 'woocommerce' ); ?>" />
-			<input type="hidden" name="woocommerce_add_payment_method" value="1" />
-		</div>
+						echo beans_close_markup( 'woo_account_payment_methods_list_item', 'li' );
 
-	</div>
+					endforeach;
 
-</form>
+				else :
+
+					echo beans_open_markup( 'woo_account_payment_methods_empty_notice', 'p' );
+
+						echo __( 'Sorry, it seems that there are no payment methods which support adding a new payment method. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' );
+
+					echo beans_close_markup( 'woo_account_payment_methods_empty_notice', 'p' );
+
+				endif;
+
+		echo beans_close_markup( 'woo_account_payment_methods_list', 'ul' );
+
+		echo beans_open_markup( 'woo_account_payment_method_submit', 'div', array( 'class' => 'form-row' ) );
+
+			wp_nonce_field( 'woocommerce-add-payment-method' );
+
+			echo beans_selfclose_markup( 'woo_account_payment_method_submit_input', 'input', array(
+				'type' => 'submit',
+				'class' => 'button alt',
+				'id' => 'place_order',
+				'value' => esc_attr_e( 'Add Payment Method', 'woocommerce' )
+			) );
+
+			echo beans_selfclose_markup( 'woo_account_payment_method_hidden_input', 'input', array(
+				'type' => 'hidden',
+				'name' => 'woocommerce_add_payment_method',
+				'value' => 1
+			) );
+
+		echo beans_close_markup( 'woo_account_payment_method_submit', 'div' );
+
+	echo beans_close_markup( 'woo_account_add_payment_wrap', 'div' );
+
+echo beans_close_markup( 'woo_account_add_payment_method_wrap', 'form' );
