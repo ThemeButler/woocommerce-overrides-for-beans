fe25db061f7a71252effdaead43bba113ad28b88
Chris Rault <chris@devignerforhire.com>
2015-12-02 18:56:29 +0200

    General cleanup

--- woocommerce/single-product/product-attributes.php
+++ woocommerce/single-product/product-attributes.php
@@ -9,61 +9,55 @@
  * @version     2.1.3
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-$has_row = false;
-$alt = 1;
+$has_row    = false;
+$alt        = 1;
 $attributes = $product->get_attributes();
 
 ob_start();
 
-echo beans_open_markup( 'woo_single_attributes_table', 'table', array( 'class' => 'shop_attributes' ) );
+beans_open_markup( 'woo_product_single_shop_attributes_table', 'table', array( 'class' => 'shop_attributes' ) );
 
 	if ( $product->enable_dimensions_display() ) :
 
 		if ( $product->has_weight() ) : $has_row = true;
 
-			echo beans_open_markup( 'woo_single_attributes_weight_row', 'tr', array(
-				if ( ( $alt = $alt * -1 ) == 1 )
-				'class' => 'alt'
-			) );
+			beans_open_markup( 'woo_product_single_shop_attributes_table_row_weight', 'tr', array( 'class' => 'shop_attributes' . ( ( $alt = $alt * -1 ) == 1 ) ? ' alt' ) );
 
-				echo beans_open_markup( 'woo_single_attributes_weight_heading', 'th' );
+				beans_open_markup( 'woo_product_single_shop_attributes_table_row_weight_heading', 'th' );
 
-					_e( 'Weight', 'woocommerce' )
+					_e( 'Weight', 'woocommerce' );
 
-				echo beans_close_markup( 'woo_single_attributes_weight_heading', 'th' );
+				beans_close_markup( 'woo_product_single_shop_attributes_table_row_weight_heading', 'th' );
 
-				echo beans_open_markup( 'woo_single_attributes_weight_value', 'td', array( 'class' => 'product_height' ) );
+				beans_open_markup( 'woo_product_single_shop_attributes_table_row_weight_heading', 'td', array( 'class' => 'product_weight' ) );
 
 					echo $product->get_weight() . ' ' . esc_attr( get_option( 'woocommerce_weight_unit' ) );
 
-				echo beans_close_markup( 'woo_single_attributes_weight_value', 'td' );
+				beans_close_markup( 'woo_product_single_shop_attributes_table_row_weight_heading', 'td' );
 
-			echo beans_close_markup( 'woo_single_attributes_weight_row', 'tr' );
+			beans_close_markup( 'woo_product_single_shop_attributes_table_row_weight', 'tr' );
 
 		endif;
 
 		if ( $product->has_dimensions() ) : $has_row = true;
 
-			echo beans_open_markup( 'woo_single_attributes_dimensions_row', 'tr', array(
-				if ( ( $alt = $alt * -1 ) == 1 )
-				'class' => 'alt'
-			) );
+			beans_open_markup( 'woo_product_single_shop_attributes_table_row_dimensions', 'tr', array( 'class' => ( ( $alt = $alt * -1 ) == 1 ) ? 'alt' ) );
 
-				echo beans_open_markup( 'woo_single_attributes_dimensions_heading', 'th' );
+				beans_open_markup( 'woo_product_single_shop_attributes_table_row_dimensions_heading', 'th' );
 
-					_e( 'Dimensions', 'woocommerce' )
+					_e( 'Dimensions', 'woocommerce' );
 
-				echo beans_close_markup( 'woo_single_attributes_dimensions_heading', 'th' );
+				beans_close_markup( 'woo_product_single_shop_attributes_table_row_dimensions_heading', 'th' );
 
-				echo beans_open_markup( 'woo_single_attributes_dimensions_value', 'td', array( 'class' => 'product_dimensions' ) );
+				beans_open_markup( 'woo_product_single_shop_attributes_table_row_quantity_value', 'td', array( 'class' => 'product_dimensions' ) );
 
 					echo $product->get_dimensions();
 
-				echo beans_close_markup( 'woo_single_attributes_dimensions_value', 'td' );
+				beans_close_markup( 'woo_product_single_shop_attributes_table_row_quantity_value', 'td' );
 
-			echo beans_close_markup( 'woo_single_attributes_weight_row', 'tr' );
+			beans_close_markup( 'woo_product_single_shop_attributes_table_row_dimensions', 'tr' );
 
 		endif;
 
@@ -76,23 +70,19 @@ echo beans_open_markup( 'woo_single_attributes_table', 'table', array( 'class' =
 			continue;
 
 		else :
-
 			$has_row = true;
 
 		endif;
 
-	echo beans_open_markup( 'woo_single_attributes_terms_row', 'tr', array(
-		if ( ( $alt = $alt * -1 ) == 1 )
-		'class' => 'alt'
-	) );
+		beans_open_markup( 'woo_product_single_shop_attributes_table_row_attributes', 'tr', array( 'class' => ( ( $alt = $alt * -1 ) == 1 ) ? 'alt' ) );
 
-		echo beans_open_markup( 'woo_single_attributes_terms_heading', 'th' );
+			beans_open_markup( 'woo_product_single_shop_attributes_table_row_attributes_heading', 'th' );
 
-			echo wc_attribute_label( $attribute['name'] );
+				echo wc_attribute_label( $attribute['name'] );
 
-		echo beans_close_markup( 'woo_single_attributes_terms_heading', 'th' );
+			beans_close_markup( 'woo_product_single_shop_attributes_table_row_attributes_heading', 'th' );
 
-		echo beans_open_markup( 'woo_single_attributes_terms_value', 'td', array( 'class' => 'product_dimensions' ) );
+			beans_open_markup( 'woo_product_single_shop_attributes_table_row_attributes_value', 'td' );
 
 				if ( $attribute['is_taxonomy'] ) :
 
@@ -107,13 +97,13 @@ echo beans_open_markup( 'woo_single_attributes_table', 'table', array( 'class' =
 
 				endif;
 
-		echo beans_close_markup( 'woo_single_attributes_terms_value', 'td' );
+			beans_close_markup( 'woo_product_single_shop_attributes_table_row_attributes_value', 'td' );
 
-		echo beans_close_markup( 'woo_single_attributes_terms_row', 'tr' );
+		beans_close_markup( 'woo_product_single_shop_attributes_table_row_attributes', 'tr'
 
 	endforeach;
 
-echo beans_close_markup( 'woo_single_attributes_table', 'table' );
+beans_close_markup( 'woo_product_single_shop_attributes_table', 'table' );
 
 if ( $has_row ) :
 
