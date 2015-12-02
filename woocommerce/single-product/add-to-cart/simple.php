30774c1659bd0d0366bd995e50a0afb7148755f5
Chris Rault <chris@devignerforhire.com>
2015-12-02 18:55:05 +0200

    Rewrote file using Beans markup

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -7,67 +7,50 @@
  * @version     2.1.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
 global $product;
 
-if ( ! $product->is_purchasable() ) :
-
+if ( ! $product->is_purchasable() ) {
 	return;
+}
 
-endif;
+?>
 
+<?php
+	// Availability
+	$availability      = $product->get_availability();
+	$availability_html = empty( $availability['availability'] ) ? '' : '<p class="stock ' . esc_attr( $availability['class'] ) . '">' . esc_html( $availability['availability'] ) . '</p>';
 
-// Availability
-$availability = $product->get_availability();
+	echo apply_filters( 'woocommerce_stock_html', $availability_html, $availability['availability'], $product );
+?>
 
-$availability_html = empty( $availability['availability'] ) ? '' : beans_open_markup( 'woo_single_add_to_cart_grouped_availability', 'p', array( 'class' => 'stock' ) ) . esc_attr( $availability['class'] ) . '">' . esc_html( $availability['availability'] ) . beans_close_markup( 'woo_single_add_to_cart_grouped_availability', 'p' );
+<?php if ( $product->is_in_stock() ) : ?>
 
-echo apply_filters( 'woocommerce_stock_html', $availability_html, $availability['availability'], $product );
+	<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-if ( $product->is_in_stock() ) :
+	<form class="cart" method="post" enctype='multipart/form-data'>
+	 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
-	do_action( 'woocommerce_before_add_to_cart_form' );
-
-	echo beans_open_markup( 'woo_single_add_to_cart_simple_form', 'form', array(
-		'class' => 'cart',
-		'method' => 'post',
-		'enctype' => 'multipart/form-data'
-	) );
-
-	 	do_action( 'woocommerce_before_add_to_cart_button' );
-
-	 	if ( ! $product->is_sold_individually() ) :
-
+	 	<?php
+	 		if ( ! $product->is_sold_individually() ) {
 	 			woocommerce_quantity_input( array(
-
 	 				'min_value'   => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
 	 				'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product ),
 	 				'input_value' => ( isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : 1 )
+	 			) );
+	 		}
+	 	?>
 
-				) );
+	 	<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->id ); ?>" />
 
-	 		endif;
+	 	<button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 
-			echo beans_selfclose_markup( 'woo_single_add_to_cart_simple_input_hidden', 'button', array(
-				'type' => 'hidden',
-				'name' => 'add-to-cart',
-				'value' => esc_attr( $product->id )
-			) );
+		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+	</form>
 
-		echo beans_open_markup( 'woo_single_add_to_cart_simple_submit_button', 'button', array(
-			'type' => 'submit',
-			'class' => 'single_add_to_cart_button button alt'
-		) );
+	<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
 
-			echo esc_html( $product->single_add_to_cart_text() );
-
-		echo beans_close_markup( 'woo_single_add_to_cart_grouped_tbody_submit_button', 'button' );
-
-		do_action( 'woocommerce_after_add_to_cart_button' );
-
-	echo beans_open_markup( 'woo_single_add_to_cart_simple_form', 'form' );
-
-	do_action( 'woocommerce_after_add_to_cart_form' );
-
-endif;
+<?php endif; ?>
