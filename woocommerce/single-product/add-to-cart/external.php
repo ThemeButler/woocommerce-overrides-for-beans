108fa0f070ae51c0ac71c35c377d478d80f8cb75
Chris Rault <chris@devignerforhire.com>
2015-12-03 12:23:52 +0200

    Revert "General cleanup"
    This reverts commit e3e0040046e778ee3a78bc6706e31421208c1668.

--- woocommerce/single-product/add-to-cart/external.php
+++ woocommerce/single-product/add-to-cart/external.php
@@ -7,16 +7,18 @@
  * @version     2.1.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-?>
+do_action( 'woocommerce_before_add_to_cart_button' );
 
-<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+echo beans_open_markup( 'woo_add_to_cart_external_wrap', 'p', array( 'class' => 'cart' ) );
 
-<p class="cart">
-	<a href="<?php echo esc_url( $product_url ); ?>" rel="nofollow" class="single_add_to_cart_button button alt"><?php echo esc_html( $button_text ); ?></a>
-</p>
+	echo beans_open_markup( 'woo_add_to_cart_external_link', 'a', array(
+		'href' => esc_url( $product_url ),
+		'rel' => 'nofollow',
+		'class' => 'single_add_to_cart_button button alt'
+	 ) ) . esc_html( $button_text ) . beans_close_markup( 'woo_add_to_cart_external_link', 'a' );
 
-<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+echo beans_close_markup( 'woo_add_to_cart_external_wrap', 'p' );
+
+do_action( 'woocommerce_after_add_to_cart_button' );
