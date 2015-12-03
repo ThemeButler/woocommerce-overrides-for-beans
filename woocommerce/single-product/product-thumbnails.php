1bab67d2ac5f10fd9146d3833cefd5f27a4cc5c1
Chris Rault <chris@devignerforhire.com>
2015-12-03 11:34:08 +0200

    Bug fixes and general cleanup

--- woocommerce/single-product/product-thumbnails.php
+++ woocommerce/single-product/product-thumbnails.php
@@ -7,7 +7,7 @@
  * @version     2.3.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $post, $product, $woocommerce;
 
@@ -17,7 +17,7 @@ if ( $attachment_ids ) :
 	$loop 		= 0;
 	$columns 	= apply_filters( 'woocommerce_product_thumbnails_columns', 3 );
 
-	echo beans_open_markup( 'woo_single_thumbnails_wrap', 'div', array( 'class' => 'thumbnails columns-' . $columns ) );
+	echo beans_open_markup( 'woo_product_single_thumbs_wrap', 'div', array( 'class' => 'thumbnails columns-' . $columns ) );
 
 		foreach ( $attachment_ids as $attachment_id ) :
 
@@ -34,28 +34,22 @@ if ( $attachment_ids ) :
 			if ( ! $image_link )
 				continue;
 
-			$image_title = esc_attr( get_the_title( $attachment_id ) );
+			$image_title 	= esc_attr( get_the_title( $attachment_id ) );
 			$image_caption 	= esc_attr( get_post_field( 'post_excerpt', $attachment_id ) );
 
-			$image = wp_get_attachment_image( $attachment_id, apply_filters( 'single_product_small_thumbnail_size', 'shop_thumbnail' ), 0, $attr = array(
+			$image       = wp_get_attachment_image( $attachment_id, apply_filters( 'single_product_small_thumbnail_size', 'shop_thumbnail' ), 0, $attr = array(
 				'title'	=> $image_title,
-				'alt' => $image_title
-			) );
+				'alt'	=> $image_title
+				) );
 
 			$image_class = esc_attr( implode( ' ', $classes ) );
 
-			echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', sprintf( beans_open_markup( 'woo_single_gallery_image_link', 'a', array(
-				'href' => '%s',
-				'itemprop' => 'image',
-				'class' => 'woocommerce-main-image zoom',
-				'title' => '%s',
-				'data-rel' => 'prettyPhoto[product-gallery]'
-			) ) . '%s' . beans_close_markup( 'woo_single_gallery_image_link', 'a' ), $image_link, $image_class, $image_caption, $image ), $attachment_id, $post->ID, $image_class );
+			echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', sprintf( beans_open_markup( 'woo_product_single_thumb_link', 'a', array( 'href' => '%s', 'class' => '%s', 'title' => '%s', 'data-rel' => 'prettyPhoto[product-gallery]' ) ) . '%s' . beans_close_markup( 'woo_product_single_thumb_link', 'a' ), $image_link, $image_class, $image_caption, $image ), $attachment_id, $post->ID, $image_class );
 
 			$loop++;
 
 		endforeach;
 
-	echo beans_close_markup( 'woo_single_thumbnails_wrap', 'div' );
+	echo beans_close_markup( 'woo_product_single_thumbs_wrap', 'div' );
 
 endif;
