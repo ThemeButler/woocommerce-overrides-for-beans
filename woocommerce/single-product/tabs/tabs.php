8d71cd216832dfc503c0c4ce5ea8ff0ee125e41a
Chris Rault <chris@devignerforhire.com>
2015-12-03 11:36:10 +0200

    Refactored the tabs markup for better consistency

--- woocommerce/single-product/tabs/tabs.php
+++ woocommerce/single-product/tabs/tabs.php
@@ -20,39 +20,36 @@ $tabs = apply_filters( 'woocommerce_product_tabs', array() );
 
 if ( ! empty( $tabs ) ) :
 
-	echo beans_open_markup( 'woo_tabs_menu_wrap', 'div', array( 'class' => 'woocommerce-tabs wc-tabs-wrapper' ) );
+	echo beans_open_markup( 'woo_product_single_tabs_wrap', 'div', array( 'class' => 'woocommerce-tabs wc-tabs-wrapper' ) );
 
-		echo beans_open_markup( 'woo_tabs_menu_wrap_list', 'ul', array( 'class' => 'tabs wc-tabs' ) );
+		echo beans_open_markup( 'woo_product_single_tabs_wrap_list', 'ul', array( 'class' => 'tabs wc-tabs' ) );
 
 			foreach ( $tabs as $key => $tab ) :
 
-				echo beans_open_markup( 'woo_tabs_menu_wrap_list_item', 'li', array( 'class' => esc_attr( $key ) ) );
+				echo beans_open_markup( 'woo_product_single_tabs_wrap_list_item', 'li', array( 'class' => esc_attr( $key ) . '_tab' ) );
 
-					echo beans_open_markup( 'woo_tabs_menu_wrap_list_item_link', 'a', array( 'href' => '#tab-' . esc_attr( $key ) ) );
+					echo beans_open_markup( 'woo_product_single_tabs_wrap_list_item_link', 'a', array( 'href' => '#tab-' . esc_attr( $key ) ) );
 
 						echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', esc_html( $tab['title'] ), $key );
 
-					echo beans_close_markup( 'woo_tabs_menu_wrap_list_item_link', 'a' );
+					echo beans_close_markup( 'woo_product_single_tabs_wrap_list_item_link', 'a' );
 
-				echo beans_close_markup( 'woo_tabs_menu_wrap_list_item', 'li' );
+				echo beans_close_markup( 'woo_product_single_tabs_wrap_list_item', 'li' );
 
 			endforeach;
 
-		echo beans_close_markup( 'woo_tabs_menu_wrap_list', 'ul' );
+		echo beans_close_markup( 'woo_product_single_tabs_wrap_list', 'ul' );
 
 		foreach ( $tabs as $key => $tab ) :
 
-			echo beans_open_markup( 'woo_tabs_panel_' . esc_attr( $key ), 'div', array(
-				'class' => 'panel entry-content wc-tab',
-				'id' => 'tab-' . esc_attr( $key )
-			) );
+			echo beans_open_markup( 'woo_product_single_tabs_wrap_tab', 'div', array( 'class' => 'panel entry-content wc-tab', 'id' => 'tab-' . esc_attr( $key ) ) );
 
 				call_user_func( $tab['callback'], $key, $tab );
 
-			echo beans_close_markup( 'woo_tabs_panel_' . esc_attr( $key ), 'div' );
+			echo beans_close_markup( 'woo_product_single_tabs_wrap_tab', 'div' );
 
 		endforeach;
 
-	echo beans_close_markup( 'woo_tabs_menu_wrap', 'div' );
+	echo beans_close_markup( 'woo_product_single_tabs_wrap', 'div' );
 
 endif;
